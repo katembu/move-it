dac6b53fef2075e4e62d6d2cfd6cd88bd64a56c3
reg <rgaudin@gmail.com>
2009-08-21 17:06:31 +0000

    Removed import to now removed Zone model

--- apps/drugtrack/admin.py
+++ apps/drugtrack/admin.py
@@ -3,9 +3,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext as _
-from models import Zone, Facility, Provider, User, Patient
+from models import Facility, Provider, User, Patient
 
 admin.site.register(Provider)
 admin.site.register(Patient)
-admin.site.register(Zone)
 admin.site.register(Facility)
