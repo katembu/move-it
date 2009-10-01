401acef87202e812e7fcdc3a5e67efd10f34224e
Dickson Ukang'a <ukanga@shiver.(none)>
2009-10-01 06:36:41 +0300

    Modified app.py to behave like a standard mctc app.py i.e using Keyworder, having authentication function and a HandlerFailed Exception Handler

--- apps/deathform/app.py
+++ apps/deathform/app.py
@@ -1,22 +1,76 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
 import rapidsms
+from rapidsms.parsers.keyworder import Keyworder
+
+from mctc.models.logs import MessageLog
+from mctc.models.general import Provider
+
+def _(txt): return txt
+
+def authenticated (func):
+    def wrapper (self, message, *args):
+        if message.sender:
+            return func(self, message, *args)
+        else:
+            message.respond(_("%s is not a registered number.")
+                            % message.peer)
+            return True
+    return wrapper
+
+class HandlerFailed (Exception):
+    pass
 
 class App (rapidsms.app.App):
+    MAX_MSG_LEN = 140
+    keyword = Keyworder()
     def start (self):
         """Configure your app in the start phase."""
         pass
 
     def parse (self, message):
-        """Parse and annotate messages in the parse phase."""
-        pass
+        # allow authentication to occur when http tester is used
+        if message.peer[:3] == '254':
+            mobile = "+" + message.peer
+        else:
+            mobile = message.peer 
+        provider = Provider.by_mobile(mobile)
+        if provider:
+            message.sender = provider.user
+        else:
+            message.sender = None
+        message.was_handled = False
 
     def handle (self, message):
-        """Add your main application logic in the handle phase."""
-        pass
+        try:
+            func, captures = self.keyword.match(self, message.text)
+        except TypeError:
+            # didn't find a matching function
+            # make sure we tell them that we got a problem
+            message.respond(_("Unknown or incorrectly formed command: %(msg)s... Please re-check your message") % {"msg":message.text[:10]})
+            
+            return False
+        try:
+            handled = func(self, message, *captures)
+        except HandlerFailed, e:
+            message.respond(e.message)
+            
+            handled = True
+        except Exception, e:
+            # TODO: log this exception
+            # FIXME: also, put the contact number in the config
+            message.respond(_("An error occurred. Please call 0733202270."))
+            raise
+        message.was_handled = bool(handled)
+        return handled
 
     def cleanup (self, message):
-        """Perform any clean up after all handlers have run in the
-           cleanup phase."""
-        pass
+        log = MessageLog(mobile=message.peer,
+                         sent_by=message.sender,
+                         text=message.text,
+                         was_handled=message.was_handled)
+        log.save()
 
     def outgoing (self, message):
         """Handle outgoing message notifications."""
