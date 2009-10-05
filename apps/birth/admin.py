fb52697c1e2ae3c559064459f0fb50f996999778
Dickson Ukang'a <ukanga@shiver.(none)>
2009-10-05 18:02:07 +0300

    Added the admin views for Birth Reports app

---
+++ apps/birth/admin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.contrib import admin
+from birth.models import ReportBirth
+
+class ReportBirthAdmin(admin.ModelAdmin):
+    list_display = ("case", "display_name", "display_dob", "weight", "entered_at", "complications")
+    search_fields = ['case__first_name', 'case__last_name', 'provider__mobile']
+    list_filter = ("weight",)
+    ordering = ('-entered_at',)
+admin.site.register(ReportBirth, ReportBirthAdmin)
