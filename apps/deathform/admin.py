853eebc9c3bf6b0b84cbc90b4c089b1df7d40eba
Dickson Ukang'a <ukanga@shiver.(none)>
2009-10-05 03:46:43 +0300

    Added an admin class for ReportDeath Model to allow fine tuning of the admin section reporting

--- apps/deathform/admin.py
+++ apps/deathform/admin.py
@@ -4,4 +4,9 @@
 from django.contrib import admin
 from deathform.models.general import ReportDeath
 
-admin.site.register(ReportDeath)
+class ReportDeathAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "gender", "cause", "location", "dod", "entered_at", "description")
+    search_fields = ['first_name', 'last_name', 'provider__mobile']
+    list_filter = ("dod",)
+    ordering = ('-dod',)
+admin.site.register(ReportDeath, ReportDeathAdmin)
