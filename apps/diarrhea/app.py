9000d1ef3c4b108754eaab0dbff7d6eb73c72f4e
Dickson Ukang'a <ukanga@shiver.(none)>
2009-10-17 11:26:00 +0300

    Migrated Diarrhea logic/functions from mctc app to diarrhea app

--- apps/diarrhea/app.py
+++ apps/diarrhea/app.py
@@ -1,22 +1,72 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
 import rapidsms
+from rapidsms.parsers.keyworder import Keyworder
+
+from django.utils.translation import ugettext as _
+
+from mctc.models.logs import MessageLog, log
+from mctc.models.general import Case
+from models import ReportDiarrhea
+
+import re, datetime
+
+find_diagnostic_re = re.compile('( -[\d\.]+)' ,re.I)
+find_lab_re =  re.compile('(/[A-Z]+)([\+-])(\d*:?)', re.I)
+
+
+def registered (func):
+    def wrapper (self, message, *args):
+        if message.persistant_connection.reporter:
+            return func(self, message, *args)
+        else:
+            message.respond(_(u"Sorry, only registered users can access this program."))
+            return True
+    return wrapper
+
+class HandlerFailed (Exception):
+    pass
 
 class App (rapidsms.app.App):
+    MAX_MSG_LEN = 140
+    keyword = Keyworder()
     def start (self):
         """Configure your app in the start phase."""
         pass
 
-    def parse (self, message):
-        """Parse and annotate messages in the parse phase."""
-        pass
+    def parse (self, message):        
+        message.was_handled = False
 
     def handle (self, message):
-        """Add your main application logic in the handle phase."""
-        pass
+        try:
+            func, captures = self.keyword.match(self, message.text)
+        except TypeError:
+            # didn't find a matching function
+            # make sure we tell them that we got a problem
+            #message.respond(_("Unknown or incorrectly formed command: %(msg)s... Please re-check your message") % {"msg":message.text[:10]})
+            
+            return False
+        try:
+            handled = func(self, message, *captures)
+        except HandlerFailed, e:
+            message.respond(e.message)
+            
+            handled = True
+        except Exception, e:
+            # TODO: log this exception
+            # FIXME: also, put the contact number in the config
+            message.respond(_("An error occurred. Please call 0733202270."))
+            raise
+        message.was_handled = bool(handled)
+        return handled
 
     def cleanup (self, message):
-        """Perform any clean up after all handlers have run in the
-           cleanup phase."""
-        pass
+        log = MessageLog(mobile=message.peer,
+                         sent_by=message.sender,
+                         text=message.text,
+                         was_handled=message.was_handled)
+        log.save()
 
     def outgoing (self, message):
         """Handle outgoing message notifications."""
@@ -25,3 +75,107 @@ class App (rapidsms.app.App):
     def stop (self):
         """Perform global app cleanup when the application is stopped."""
         pass
+
+    def find_case (self, ref_id):
+        try:
+            return Case.objects.get(ref_id=int(ref_id))
+        except Case.DoesNotExist:
+            raise HandlerFailed(_("Case +%s not found.") % ref_id)
+
+    # DIARRHEA
+    # follow up on diarrhea
+    @keyword(r'ors \+(\d+) ([yn])')
+    @registered
+    def followup_diarrhea(self, message, ref_id, is_ok):
+        case    = self.find_case(ref_id)
+        is_ok   = True if is_ok == "y" else False
+
+        reporter = message.persistant_connection.reporter
+        report = ReportDiarrhea.objects.get(case=case)
+        
+        if is_ok:
+            report.status   = ReportDiarrhea.HEALTHY_STATUS
+            report.save()
+        else:
+            report.status   = ReportDiarrhea.SEVERE_STATUS
+            report.save()
+
+            info = report.case.get_dictionary()
+            info.update(report.get_dictionary())
+   
+            msg = _("%(diagnosis_msg)s. +%(ref_id)s %(last_name)s, %(first_name_short)s, %(gender)s/%(months)s (%(guardian)s). %(days)s, %(ors)s") % info
+            if report.observed.all().count() > 0: msg += ", " + info["observed"]
+            
+            message.respond("DIARRHEA> " + msg)
+            
+        
+        
+        if report.status in (report.MODERATE_STATUS,
+                           report.SEVERE_STATUS,
+                           report.DANGER_STATUS):
+            alert = _("@%(username)s reports %(msg)s") % {"username":reporter.alias, "msg":msg}
+            recipients = [reporter]
+            for query in (Provider.objects.filter(alerts=True),
+                          Provider.objects.filter(clinic=provider.clinic)):
+                for recipient in query:
+                    if recipient in recipients: continue
+                    recipients.append(recipient)
+                    message.forward(recipient.mobile, alert)
+                 
+        log(case, "diarrhea_fu_taken")
+        return True
+
+    @keyword(r'ors \+(\d+) ([yn]) (\d+)\s?([a-z\s]*)')
+    @registered
+    def report_diarrhea(self, message, ref_id, ors, days, complications):
+        case = self.find_case(ref_id)
+
+        ors     = True if ors == "y" else False
+        days    = int(days)
+
+        observed, choices = self.get_diarrheaobservations(complications)
+        self.delete_similar(case.reportdiarrhea_set)
+
+        reporter = message.persistant_connection.reporter
+        report = ReportDiarrhea(case=case, reporter=reporter, ors=ors, days=days)
+        report.save()
+        for obs in observed:
+            report.observed.add(obs)
+        report.diagnose()
+        report.save()
+
+        choice_term = dict(choices)
+
+        info = case.get_dictionary()
+        info.update(report.get_dictionary())
+
+        msg = _("%(diagnosis_msg)s. +%(ref_id)s %(last_name)s, %(first_name_short)s, %(gender)s/%(months)s (%(guardian)s). %(days)s, %(ors)s") % info
+
+        if observed: msg += ", " + info["observed"]
+
+        message.respond("DIARRHEA> " + msg)
+        
+
+        if report.status in (report.MODERATE_STATUS,
+                           report.SEVERE_STATUS,
+                           report.DANGER_STATUS):
+            alert = _("@%(username)s reports %(msg)s") % {"username":provider.user.username, "msg":msg}
+            recipients = [provider]
+            for query in (Provider.objects.filter(alerts=True),
+                          Provider.objects.filter(clinic=provider.clinic)):
+                for recipient in query:
+                    if recipient in recipients: continue
+                    recipients.append(recipient)
+                    message.forward(recipient.mobile, alert)
+                    
+        log(case, "diarrhea_taken")
+        return True            
+
+    def delete_similar(self, queryset):
+        try:
+            last_report = queryset.latest("entered_at")
+            if (datetime.datetime.now() - last_report.entered_at).days == 0:
+                # last report was today. so delete it before filing another.
+                last_report.delete()
+        except models.ObjectDoesNotExist:
+            pass
--- apps/mctc/models/reports.py
+++ apps/mctc/models/reports.py
@@ -50,18 +50,6 @@ class Observation(models.Model):
     def __unicode__(self):
         return self.name
 
-class DiarrheaObservation(models.Model):
-    uid = models.CharField(max_length=15, primary_key=True)
-    name = models.CharField(max_length=255)
-    letter = models.CharField(max_length=2, unique=True)
-
-    class Meta:
-        app_label = "mctc"
-        ordering = ("name",)
-
-    def __unicode__(self):
-        return self.name
-        
 class ReportMalaria(Report, models.Model):
     class Meta:
         get_latest_by = 'entered_at'
@@ -246,69 +234,6 @@ class ReportMalnutrition(Report, models.Model):
             return None 
 
 
-class ReportDiarrhea(Report, models.Model):
-    
-    MODERATE_STATUS         = 1
-    DANGER_STATUS           = 2
-    SEVERE_STATUS           = 3
-    HEALTHY_STATUS          = 4
-    STATUS_CHOICES = (
-        (MODERATE_STATUS,   _('Moderate')),
-        (DANGER_STATUS,     _('Danger')),
-        (SEVERE_STATUS,     _('Severe')),
-        (HEALTHY_STATUS,    _("Healthy")),
-    )
-
-    case        = models.ForeignKey(Case, db_index=True)
-    provider    = models.ForeignKey(Provider, db_index=True)
-    entered_at  = models.DateTimeField(db_index=True)
-    ors         = models.BooleanField()
-    days        = models.IntegerField(_("Number of days"))    
-    observed    = models.ManyToManyField(DiarrheaObservation, blank=True)
-    status      = models.IntegerField(choices=STATUS_CHOICES, db_index=True, blank=True, null=True)
-    
-    class Meta:
-        app_label = "mctc"
-        verbose_name = "Diarrhea Report"
-        verbose_name_plural = "Diarrhea Reports"
-        get_latest_by = 'entered_at'
-        ordering = ("-entered_at",)
-
-    def get_dictionary(self):
-        return {
-            'ors'       : "ORS: %s" % ("yes" if self.ors else "no"),
-            'days'      : "Days: %d" % self.days,
-            'observed'  : ", ".join([k.name for k in self.observed.all()]),
-            'diagnosis' : self.get_status_display(),
-            'diagnosis_msg' : self.diagnosis_msg(),
-        }
-                               
-    def __unicode__ (self):
-        return "#%d" % self.id
-
-    def diagnose (self):
-        if self.days >= 3 or self.observed.all().count() > 0:
-            self.status = ReportDiarrhea.DANGER_STATUS
-        else:
-            self.status = ReportDiarrhea.MODERATE_STATUS
-
-    def diagnosis_msg(self):
-        if self.status == ReportDiarrhea.MODERATE_STATUS:
-            msg = "MOD Patient should take ORS."
-        elif self.status == ReportDiarrhea.SEVERE_STATUS:
-            msg = "SEV Patient must be referred at clinic."
-        elif self.status == ReportDiarrhea.DANGER_STATUS:
-            msg = "DANG Patient must go to Clinic."
-        else:
-            msg = "HEAL Patient not in danger."
-   
-        return msg
-
-    def save(self, *args):
-        if not self.id:
-            self.entered_at = datetime.now()
-        super(ReportDiarrhea, self).save(*args)
-        
 class ReportCHWStatus(Report, models.Model):
     class Meta:
         verbose_name = "CHW Perfomance Report"
