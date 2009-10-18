b07db4ca1244e71b29aa2667c22210d50266641c
Dickson Ukang'a <ukanga@shiver.(none)>
2009-10-18 09:51:51 +0300

    Added admin interface for measles app

---
+++ apps/measles/admin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.contrib import admin
+
+from models import ReportMeasles
+class ReportMeaslesAdmin(admin.ModelAdmin):
+    list_display = ("case", "reporter", "taken", "entered_at", "location")
+    list_filter = ("taken",)
+    search_fields = ['location__name', ]
+    
+admin.site.register(ReportMeasles, ReportMeaslesAdmin)
