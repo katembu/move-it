d94d2bbc8bfe61628ec0e5705022dcf77171bdc7
Dickson Ukang'a <ukanga@shiver.(none)>
2009-11-03 09:06:34 +0300

    added a list filter to MessageLog admin view

--- apps/mctc/admin.py
+++ apps/mctc/admin.py
@@ -15,7 +15,7 @@ admin.site.register(Case, CaseAdmin)
 
 class MessageLogAdmin(admin.ModelAdmin):
     list_display = ("sent_by_name", "mobile", "text", "created_at", "was_handled")
-    #list_filter = ("was_handled","sent_by")
+    list_filter = ("was_handled","created_at")
     search_fields = [ 'mobile','text', ]
     
 admin.site.register(MessageLog, MessageLogAdmin)
