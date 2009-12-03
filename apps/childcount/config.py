fe3d5ba1ac3d421e1c34e866384e72f95bec8138
reg <reg@bibi.(none)>
2009-12-03 12:01:12 +0000

    updated docstrings

--- apps/childcount/config.py
+++ apps/childcount/config.py
@@ -1,8 +1,6 @@
-
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-'''
-@author: ukanga
-'''
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
 title = "ChildCount"
 tab_link = "/childcount/reports"
