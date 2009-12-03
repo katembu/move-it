1a95bc7922f7587e407592c4e299efe1fc669043
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-03 19:15:37 +0300

    fixed E203 whitespace before ','

--- apps/diarrhea/app.py
+++ apps/diarrhea/app.py
@@ -13,7 +13,7 @@ from models import ReportDiarrhea, DiarrheaObservation
 
 import re, datetime
 
-find_diagnostic_re = re.compile('( -[\d\.]+)' ,re.I)
+find_diagnostic_re = re.compile('( -[\d\.]+)',re.I)
 find_lab_re =  re.compile('(/[A-Z]+)([\+-])(\d*:?)', re.I)
 
 
