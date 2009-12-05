d18dca68f5f433ea5418e4d4a776e6b6b45b6129
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-05 12:20:59 +0300

    display num of unique muacs done

--- apps/childcount/models/reports.py
+++ apps/childcount/models/reports.py
@@ -115,7 +115,8 @@ class ReportCHWStatus(Report, models.Model):
                     muac_percentage = 0
                 else:
                     muac_percentage  = round(float(float(num_muac)/float(num_cases))*100, 0)
-                p['num_muac_reports'] = "%d %d%% (%s/%s)"%(num_muac, muac_percentage, num_muac, num_cases)
+                #p['num_muac_reports'] = "%d %d%% (%s/%s)"%(num_muac, muac_percentage, num_muac, num_cases)
+                p['num_muac_reports'] = num_muac
                 sms_sent = MessageLog.count_by_provider(provider, duration_end, duration_start)
                 clinic_sent = clinic_sent + sms_sent
                 p['sms_sent'] = sms_sent
@@ -151,7 +152,8 @@ class ReportCHWStatus(Report, models.Model):
                 muac_percentage = 0
             else:
                 muac_percentage  = round(float(float(num_muac)/float(num_cases))*100, 0)
-            p['num_muac_reports'] = "%d %% (%s/%s)"%(muac_percentage, num_muac, num_cases)
+            #p['num_muac_reports'] = "%d %% (%s/%s)"%(muac_percentage, num_muac, num_cases)
+            p['num_muac_reports'] = num_muac
             p['sms_sent'] = clinic_sent
             p['sms_processed'] = clinic_processed
             p['sms_refused'] = clinic_refused
