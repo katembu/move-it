90eaa96684eb0e92050a10fe97fa20bad398e9fb
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 00:42:48 +0300

    updated docstrings

--- apps/diagnosis/app.py
+++ apps/diagnosis/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 import rapidsms
 from rapidsms.parsers.keyworder import Keyworder
