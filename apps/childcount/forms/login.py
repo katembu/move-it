ae730dad7ae4d8fffe6f7ff1e770dc87062e1850
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-06 19:49:35 +0300

    pep8

--- apps/childcount/forms/login.py
+++ apps/childcount/forms/login.py
@@ -2,14 +2,13 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from base import BaseForm
 
+
 class LoginForm(BaseForm):
     username = forms.CharField(
         label=_("Username"),
-        required=True
-    )
-    
+        required=True)
+
     password = forms.CharField(
         label=_("Password"),
         required=True,
-        widget=forms.PasswordInput()
-    )
+        widget=forms.PasswordInput())
