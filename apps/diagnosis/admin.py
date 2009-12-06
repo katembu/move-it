cb15a068aa1c9187ac3de2ebf15db5419c74e73b
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 01:07:57 +0300

    updated docstrings

--- apps/diagnosis/admin.py
+++ apps/diagnosis/admin.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from django.contrib import admin
 
-from models import Diagnosis, DiagnosisCategory,ReportDiagnosis
+from models import Diagnosis, DiagnosisCategory, ReportDiagnosis
 
 admin.site.register(Diagnosis)
 admin.site.register(DiagnosisCategory)
+
 
 class ReportDiagnosisAdmin(admin.ModelAdmin):
     list_display = ("case", "entered_at")
