b23f97f7d15b738490f61f053309f23b5e6dfd92
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 02:56:43 +0300

    updated docstrings

--- apps/mrdt/app.py
+++ apps/mrdt/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+import re
+import datetime
 
 from django.db import models
 from django.utils.translation import ugettext as _
@@ -12,7 +16,7 @@ from childcount.models.general import Case
 from childcount.models.reports import Observation
 from models import ReportMalaria
 
-import re, datetime
+
 
 def registered (func):
     def wrapper (self, message, *args):
