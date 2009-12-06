36c407e4257d21a21b2da93f83aa3dfeaf59ebff
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 02:33:37 +0300

    updated docstrings

--- apps/measles/app.py
+++ apps/measles/app.py
@@ -2,6 +2,12 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 # maintainer: ukanga
 
+'''Measles main app
+
+record vaccinated cases - measles
+get summarry report - msummary
+'''
+
 from functools import wraps
 
 from django.db import models
@@ -17,7 +23,7 @@ from reporters.models import Reporter, ReporterGroup
 from measles.models import ReportMeasles
 
 
-def registered (func):
+def registered(func):
     ''' decorator checking if sender is allowed to process feature.
 
     checks if a reporter is attached to the message
@@ -34,34 +40,62 @@ def registered (func):
             return True
     return wrapper
 
+
 class HandlerFailed (Exception):
     pass
 
+
 class App (rapidsms.app.App):
+
+    '''Measles main app
+
+    record vaccinated cases - measles
+    get summarry report - msummary
+    '''
+
     MAX_MSG_LEN = 140
     keyword = Keyworder()
     handled = False
-    def start (self):
+
+    def start(self):
         '''Configure your app in the start phase.'''
         pass
 
-    def parse (self, message):
+    def parse(self, message):
+        ''' Parse incoming messages.
+
+        flag message as not handled '''
         message.was_handled = False
 
-    def handle (self, message):
+    def handle(self, message):
+        ''' Function selector
+
+        Matchs functions with keyword using Keyworder
+        Replies formatting advices on error
+        Return False on error and if no function matched '''
         try:
             func, captures = self.keyword.match(self, message.text)
         except TypeError:
             # didn't find a matching function
             # make sure we tell them that we got a problem
-            #message.respond(_("Unknown or incorrectly formed command: %(msg)s... Please re-check your message") % {"msg":message.text[:10]})
-            
+            command_list = [method for method in dir(self) \
+                            if hasattr(getattr(self, method), "format")]
+            input_text = message.text.lower()
+            for command in command_list:
+                format = getattr(self, command).format
+                try:
+                    first_word = (format.split(" "))[0]
+                    if input_text.find(first_word) > -1:
+                        message.respond(format)
+                        return True
+                except:
+                    pass
             return False
         try:
             self.handled = func(self, message, *captures)
         except HandlerFailed, e:
             message.respond(e.message)
-            
+
             self.handled = True
         except Exception, e:
             # TODO: log this exception
@@ -71,7 +105,8 @@ class App (rapidsms.app.App):
         message.was_handled = bool(self.handled)
         return self.handled
 
-    def cleanup (self, message):
+    def cleanup(self, message):
+        ''' log message '''
         if bool(self.handled):
             log = MessageLog(mobile=message.peer,
                          sent_by=message.persistant_connection.reporter,
@@ -79,15 +114,20 @@ class App (rapidsms.app.App):
                          was_handled=message.was_handled)
             log.save()
 
-    def outgoing (self, message):
+    def outgoing(self, message):
         '''Handle outgoing message notifications.'''
         pass
 
-    def stop (self):
+    def stop(self):
         '''Perform global app cleanup when the application is stopped.'''
         pass
 
-    def find_case (self, ref_id):
+    def find_case(self, ref_id):
+        '''Find a registered case
+
+        return the Case object
+        raise HandlerFailed if case not found
+        '''
         try:
             return Case.objects.get(ref_id=int(ref_id))
         except Case.DoesNotExist:
@@ -95,32 +135,37 @@ class App (rapidsms.app.App):
 
     @keyword(r'measles ?(.*)')
     @registered
-    def measles(self, message, text):        
+    def measles(self, message, text):
+        '''Record vaccinated cases
+
+        format: measles [+PID] [+PID] [+PID]
+        '''
         reporter = message.persistant_connection.reporter
         cases, notcases = self.str_to_cases(text)
         result = ""
         for case in cases:
-            result = result + "+%s "%case.ref_id
+            result = result + "+%s " % case.ref_id
             report = ReportMeasles(case=case, reporter=reporter, taken=True)
             report.save()
         if result != "":
             msg = result + " received measles shot."
-            message.respond(_("%s")%msg)
+            message.respond(_("%s") % msg)
         if notcases:
-            nresult = "" 
+            nresult = ""
             for nc in notcases:
-                nresult = nresult +"%s "%nc
-            msg = nresult + " not found!!"            
-            message.respond(_("%s")%msg)
+                nresult = nresult + "%s " % nc
+            msg = nresult + " not found!!"
+            message.respond(_("%s") % msg)
         return True
-    
-    
-    def str_to_cases(self, text): 
-        '''
-        @text a string containing space separeted +PID, e.g +78 +98 ..or 78 98 ...
+    measles.format = "measles [+PID] [+PID] [+PID]"
+
+    def str_to_cases(self, text):
+        '''Pick PIDs and return the a list of cases they represent
+
+        @text a string containing space separeted +PID, e.g +78 +98 ..or 78 98
         @return: Case Object list, and a list of numbers that were not cases
-        '''       
-        text = text.replace("+","")
+        '''
+        text = text.replace("+", "")
         cs = text.split(" ")
         cases = []
         notcases = []
@@ -130,44 +175,46 @@ class App (rapidsms.app.App):
             except HandlerFailed:
                 notcases.append(c)
         return cases, notcases
-        
+
     @keyword(r'msummary')
     def measles_summary(self, message):
-        '''
-        Send measles summary to health facilitators
+        '''Send measles summary to health facilitators
         and those who are to receive alerts
-        '''        
+        '''
         header = u"Measles Summary by facility:"
         result = []
-        
-        summary =  ReportCHWStatus.measles_mini_summary()
+
+        summary = ReportCHWStatus.measles_mini_summary()
         tmp = header
         for info in summary:
             if info["eligible_cases"] != 0:
-                info["percentage"] = round(float(float(info["vaccinated_cases"])/float(info["eligible_cases"]))*100, 0)
+                info["percentage"] = \
+                    round(float(float(info["vaccinated_cases"]) / \
+                                float(info["eligible_cases"])) * 100, 0)
             else:
-                info["percentage"] = 0 
-            item = u" %(clinic)s: %(vaccinated_cases)s/%(eligible_cases)s %(percentage)s%%,"%info
+                info["percentage"] = 0
+            item = u" %(clinic)s: %(vaccinated_cases)s/%(eligible_cases)s "\
+                    "%(percentage)s%%," % info
             if len(tmp) + len(item) + 2 >= self.MAX_MSG_LEN:
                 result.append(tmp)
                 tmp = header
             tmp += item
         if tmp != header:
             result.append(tmp)
-        #message.forward(u"0733202270", u"Start Measles Summary...")
-            
         subscribers = Reporter.objects.all()
         for text in result:
             for subscriber in subscribers:
                 try:
-                    if subscriber.registered_self and ReporterGroup.objects.get(title='measles_summary') in subscriber.groups.only():                
+                    if subscriber.registered_self \
+                        and ReporterGroup.objects.get(title='measles_summary')\
+                             in subscriber.groups.only():
                         mobile = subscriber.connection().identity
-                        message.forward(mobile, _("%s")%text)          
+                        message.forward(mobile, _("%s") % text)
                 except:
                     '''
-                    FIXME: The group might have not been created, need to alert the developer/in charge here
+                    FIXME: The group might have not been created,
+                    need to alert the developer/in charge here
                     '''
                     pass
-                
-        #message.forward(u"0733202270", u"...Measles Summary End")
         return True
+    measles_summary.format = "msummary"
