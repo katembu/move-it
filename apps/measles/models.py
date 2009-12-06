a459162842008223c6be83629d5c87e015d24c85
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 02:06:48 +0300

    updated docstrings

--- apps/measles/models.py
+++ apps/measles/models.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
-# Records cases which have received measles shot
+'''Measles app Models
+
+ReportMeasles - Records cases which have received measles shot
+'''
 
 from django.db import models
 
@@ -9,30 +13,41 @@ from datetime import datetime
 
 from childcount.models.general import Case
 from reporters.models import Reporter
- 
+
+
 class ReportMeasles(models.Model):
+
+    '''Records cases which have received measles shot'''
+
     class Meta:
         get_latest_by = 'entered_at'
         ordering = ("-entered_at",)
         app_label = "measles"
         verbose_name = "Measles Report"
         verbose_name_plural = "Measles Reports"
-    
+
     case = models.ForeignKey(Case, db_index=True)
     reporter = models.ForeignKey(Reporter, db_index=True)
     entered_at = models.DateTimeField(db_index=True)
     taken = models.BooleanField(db_index=True)
-    
+
     def save(self, *args):
+        '''Set entered_at field with current time and then save record'''
         if not self.id:
             self.entered_at = datetime.now()
         super(ReportMeasles, self).save(*args)
-        
+
     def location(self):
+        '''get the location of the child,case'''
         return self.case.location
 
     @classmethod
-    def is_vaccinated(cls,case):
+    def is_vaccinated(cls, case):
+        '''Checks if the case/child is vaccinated
+
+        returns True if child is Vaccinated,
+         otherwise returns false
+        '''
         try:
             rpt = cls.objects.filter(case=case).latest()
             if rpt:
@@ -40,17 +55,23 @@ class ReportMeasles(models.Model):
             return False
         except models.ObjectDoesNotExist:
             return False
-    
+
     @classmethod
-    def get_vaccinated(cls,reporter):
+    def get_vaccinated(cls, reporter):
+        '''Gets a list of vaccinated cases'''
         try:
-            rpt = cls.objects.values("case").distinct().filter(reporter=reporter)
-            return rpt            
+            rpt = cls.objects.values("case").distinct().\
+                filter(reporter=reporter)
+            return rpt
         except models.ObjectDoesNotExist:
             return False
-    
+
     @classmethod
     def summary_by_location(cls):
+        '''gets measles summary by clinic
+
+        counts the number of vaccinated children per clinic
+        '''
         try:
             rpts = cls.objects.order_by("case__location")
             zones = []
@@ -61,11 +82,11 @@ class ReportMeasles(models.Model):
                     zcount = 0
                     l = z.case.location
                 if l != z.case.location:
-                    zones.append((l.name,zcount))
+                    zones.append((l.name, zcount))
                     zcount = 0
                     l = z.case.location
                 zcount += 1
-            zones.append((l.name,zcount))
+            zones.append((l.name, zcount))
             return zones
         except:
             pass
