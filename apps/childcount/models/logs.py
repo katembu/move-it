40dedd8589fe66a343d61cac3ba532be102dd55b
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-06 19:48:00 +0300

    pep8

--- apps/childcount/models/logs.py
+++ apps/childcount/models/logs.py
@@ -14,7 +14,6 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
 
 from datetime import datetime, date
 
