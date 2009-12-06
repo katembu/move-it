f2d26b627d089c3c1346bc20737949014f70f0d5
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-06 19:54:48 +0300

    pep8

--- apps/childcount/forms/general.py
+++ apps/childcount/forms/general.py
@@ -9,26 +9,26 @@ from reporters.models import Reporter, ReporterGroup
 
 from urllib import quote
 
+
 class MessageForm(BaseForm):
     message = forms.CharField(
         label=_("Message text"),
         required=True,
 # oh if only this worked properly
-#        widget = forms.Textarea(attrs={"class":"span-6", "style": "height; 2em"})
+#        widget = forms.Textarea(attrs={"class":"span-6", "style":
+#            "height; 2em"})
     )
-    
+
     groups = forms.MultipleChoiceField(
         label=_("Groups"),
         required=False,
-        widget=forms.CheckboxSelectMultiple()
-    )
+        widget=forms.CheckboxSelectMultiple())
 
     users = forms.MultipleChoiceField(
         label=_("Users"),
-        required=False,        
-        widget=forms.CheckboxSelectMultiple()
-    )
-    
+        required=False,
+        widget=forms.CheckboxSelectMultiple())
+
     def clean(self):
         data = self.cleaned_data
         if data.get("message"):
@@ -38,29 +38,34 @@ class MessageForm(BaseForm):
             try:
                 quote(data.get("message"))
             except KeyError:
-                msg = _("You can only specify ASCII characters in the message.")
-                self._errors["message"] = ErrorList([msg,])
+                msg = _("You can only specify ASCII characters "\
+                        "in the message.")
+                self._errors["message"] = ErrorList([msg, ])
         if not data.get("groups") and not data.get("users"):
             msg = _("You must specify either one or more groups or users.")
-            self._errors["groups"] = ErrorList([msg,])
-            
+            self._errors["groups"] = ErrorList([msg, ])
+
         return super(MessageForm, self).clean()
-    
+
     def __init__(self, *args, **kw):
         super(MessageForm, self).__init__(*args, **kw)
-        
+
         groups = [(str(g.id), g.title) for g in ReporterGroup.objects.all()]
         users = []
         for p in Reporter.objects.all().order_by('last_name'):
             if p.first_name and p.last_name:
-                users.append([str(p.id), "%s %s" % (p.first_name, p.last_name)])
+                users.append([str(p.id), "%s %s" % (p.first_name, \
+                                                    p.last_name)])
             else:
-                users.append([str(p.id), "%s (name not entered)" % p.connection().identity])
-                
+                users.append([str(p.id), "%s (name not entered)"\
+                               % p.connection().identity])
+
         self.fields["groups"].choices = groups
         self.fields["users"].choices = users
 
+
 class CaseForm(BaseModelForm):
+
     class Meta:
         model = Case
         exclude = ('created_at', 'updated_at')
