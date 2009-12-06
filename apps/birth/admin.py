69de0f2d26b3958a965157020673622fae02612e
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-06 12:48:54 +0300

    updated docstrings

--- apps/birth/admin.py
+++ apps/birth/admin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from django.contrib import admin
 from birth.models import ReportBirth
