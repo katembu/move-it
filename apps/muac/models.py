7f45a36379b9d0c57ca8f06205c351dadf450194
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 09:39:41 +0300

    updated docstrings

--- apps/muac/models.py
+++ apps/muac/models.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+'''Malnutrition App Models
+
+ReportMalnutrition - record malnutrition measurements
+'''
 
 from django.db import models
 from django.utils.translation import ugettext as _
@@ -10,8 +16,11 @@ from childcount.models.general import Case
 from childcount.models.reports import Observation
 from reporters.models import Reporter,ReporterGroup
 
+
 class ReportMalnutrition(models.Model):
-    
+
+    '''record malnutrition measurements'''
+
     MODERATE_STATUS         = 1
     SEVERE_STATUS           = 2
     SEVERE_COMP_STATUS      = 3
@@ -64,21 +73,24 @@ class ReportMalnutrition(models.Model):
         if not logs:
             return ""
         return (today - logs[0].entered_at.date()).days
-    
+
     def zone(self):
         return self.case.zone.name
-        
+
     def name(self):
-        return "%s %s" % (self.case.first_name, self.case.last_name) 
-        
+        '''Get the name of the case'''
+        return "%s %s" % (self.case.first_name, self.case.last_name)
+
     def provider_number(self):
+        '''get reporter's mobile phone number'''
         return self.reporter.connection().identity
-            
+
     def diagnose (self):
-        complications = [c for c in self.observed.all() if c.uid != "edema" or c.uid != "oedema"]
-        edema = "edema" in [ c.uid for c in self.observed.all() ]
+        complications = [c for c in self.observed.all() \
+                         if c.uid != "edema" or c.uid != "oedema"]
+        edema = "edema" in [c.uid for c in self.observed.all()]
         if not edema:
-            edema = "oedema" in [ c.uid for c in self.observed.all() ]
+            edema = "oedema" in [c.uid for c in self.observed.all()]
         self.status = ReportMalnutrition.HEALTHY_STATUS
         if edema or self.muac < 110:
             if complications:
@@ -88,7 +100,8 @@ class ReportMalnutrition(models.Model):
         elif self.muac < 125:
             self.status =  ReportMalnutrition.MODERATE_STATUS
 
-    def diagnosis_msg(self,lang=None):
+    def diagnosis_msg(self, lang=None):
+        '''get a diagnostic message/text'''
         if lang is None:
             lang = 'en'
         msg = {}
@@ -99,77 +112,93 @@ class ReportMalnutrition(models.Model):
             msg['fr'] = "SAM Patient a besoin d aller au poste de sante"
             msg['en'] = "SAM Patient requires OTP care"
         elif self.status == ReportMalnutrition.SEVERE_COMP_STATUS:
-            msg['fr'] = "SAM+ Patient a besoin d aller immediatement au poste de sante"
+            msg['fr'] = "SAM+ Patient a besoin d aller immediatement au "\
+                        "poste de sante"
             msg['en'] = "SAM+ Patient requires IMMEDIATE inpatient care"
         else:
             msg['fr'] = "Enfant nest pas malnutri"
             msg['en'] = "Child is not malnourished"
-        
+
         if not lang in msg:
             lang = 'en'
-   
+
         return msg[lang]
 
     def save(self, *args):
+        '''Set entered_at field with current time and then save record'''
         if not self.id:
             self.entered_at = datetime.now()
         super(ReportMalnutrition, self).save(*args)
-      
+
     def get_last_muac(self):
+        '''Get the last reported muac measurements b4 this one for this case'''
         b4 = self.entered_at.replace(day=self.entered_at.day-1)
-        
-        p = ReportMalnutrition.objects.filter(case=self.case,entered_at__lte=b4)
+
+        p = ReportMalnutrition.objects.filter(case=self.case, \
+                                              entered_at__lte=b4)
         if p:
             prev = p[0]
             info = prev.get_dictionary()
-            change = int(float(( self.muac-prev.muac)/float(self.muac))*100)
-            info.update({"reported_date": prev.entered_at.strftime("%d.%m.%y"),
-                         "percentage": change
-                         })
+            change = int(float((self.muac-prev.muac) / \
+                               float(self.muac)) * 100)
+            info.update({"reported_date": \
+                         prev.entered_at.strftime("%d.%m.%y"),
+                         "percentage": change})
             return info
         return None
-       
+
     @classmethod
-    def count_by_provider(cls,reporter, duration_end=None,duration_start=None, status=None):
-        
+    def count_by_provider(cls, reporter, duration_end=None, \
+                          duration_start=None, status=None):
         '''Count Muac reports
-        
+
         duration_start - starting date
         duration_end   - end date
         reporter - the reporter/provider
-        status - MODERATE_STATUS | SEVERE_STATUS | SEVERE_COMP_STATUS | HEALTHY_STATUS | None
-        
+        status - MODERATE_STATUS | SEVERE_STATUS | SEVERE_COMP_STATUS
+         | HEALTHY_STATUS | None
+
         '''
-        
         if reporter is None:
             return None
         try:
             if duration_start is None or duration_end is None:
                 if status is None:
-                    return cls.objects.filter(reporter=reporter).values("case").distinct().count()
+                    return cls.objects.filter(reporter=reporter).\
+                        values("case").distinct().count()
                 else:
-                    return cls.objects.filter(reporter=reporter, status=status).values("case").distinct().count()
+                    return cls.objects.filter(reporter=reporter, \
+                        status=status).values("case").distinct().count()
             if status is None:
-                return cls.objects.filter(entered_at__lte=duration_end, entered_at__gte=duration_start).filter(reporter=reporter).values("case").distinct().count()
+                return cls.objects.filter(entered_at__lte=duration_end, \
+                        entered_at__gte=duration_start).\
+                        filter(reporter=reporter).values("case").\
+                        distinct().count()
             else:
-                return cls.objects.filter(entered_at__lte=duration_end, entered_at__gte=duration_start, status=status).filter(reporter=reporter).values("case").distinct().count()
-            
+                return cls.objects.filter(entered_at__lte=duration_end, \
+                        entered_at__gte=duration_start, status=status).\
+                        filter(reporter=reporter).values("case").\
+                        distinct().count()
         except models.ObjectDoesNotExist:
             return None
-    
+
     @classmethod
     def num_reports_by_case(cls, case=None):
+        '''Count the number of recorded muac measurements for this case'''
         if case is None:
             return None
         try:
             return cls.objects.filter(case=case).count()
         except models.ObjectDoesNotExist:
             return None
-            
+
     def get_alert_recipients(self):
+        '''Get subscribers subscribed to malnutrion alerts'''
         recipients = []
         subscribers = Reporter.objects.all()
         for subscriber in subscribers:
-            if subscriber.registered_self and ReporterGroup.objects.get(title='muac_notifications') in subscriber.groups.only():
+            if subscriber.registered_self \
+                and ReporterGroup.objects.get(title='muac_notifications')\
+                 in subscriber.groups.only():
                 recipients.append(subscriber)
         return recipients
