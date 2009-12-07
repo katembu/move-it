16408643c465f0e779b3c72240136de789941d65
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 10:22:13 +0300

    updated docstrings

--- apps/takeover/app.py
+++ apps/takeover/app.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+'''Takeover app
+
+when using the httptester, it gives a number a http identity if the
+number was of a registered reporter
+'''
+
+from functools import wraps
+import re
+
+from django.utils.translation import ugettext as _
+from django.db import models
 
 import rapidsms
 from rapidsms.parsers.keyworder import Keyworder
-from django.utils.translation import ugettext as _
-from django.db import models
+
 from reporters.models import PersistantBackend, Reporter
 from childcount.models.logs import MessageLog
 
-import re
 
 class HandlerFailed (Exception):
     pass
 
-from functools import wraps
-
+'''Had this for an @wraps decorator testing
 def adeco(func):
     @wraps(func)
     def wrapper(self, *args):
@@ -26,47 +36,83 @@ def adeco(func):
 def anotherdeco(func):
     @wraps(func)
     def wrapper(self, *args):
-        print("Another decorator func")        
+        print("Another decorator func")
         print func.__doc__
         return func(self, *args)
     return wrapper
+'''
 
-def registered (func):
+
+def registered(func):
+    ''' decorator checking if sender is allowed to process feature.
+
+    checks if a reporter is attached to the message
+
+    return function or boolean '''
+
     @wraps(func)
-    def wrapper (self, message, *args):
+    def wrapper(self, message, *args):
         if message.persistant_connection.reporter:
             return func(self, message, *args)
         else:
-            message.respond(_(u"%s")%"Sorry, only registered users can access this program.")
+            message.respond(_(u"%s") % \
+                "Sorry, only registered users can access this program.")
             return True
     return wrapper
 
 
 class App (rapidsms.app.App):
+
+    '''Takeover app
+
+    when using the httptester, it gives a number a http identity if the
+    number was of a registered reporter
+    takeover @alias
+    '''
+
     MAX_MSG_LEN = 140
     keyword = Keyworder()
     handled = False
-    def start (self):
+
+    def start(self):
         '''Configure your app in the start phase.'''
         pass
 
-    def parse (self, message):        
+    def parse(self, message):
+        ''' Parse incoming messages.
+
+        flag message as not handled '''
         message.was_handled = False
 
-    def handle (self, message):
+    def handle(self, message):
+        ''' Function selector
+
+        Matchs functions with keyword using Keyworder
+        Replies formatting advices on error
+        Return False on error and if no function matched '''
         try:
             func, captures = self.keyword.match(self, message.text)
         except TypeError:
             # didn't find a matching function
             # make sure we tell them that we got a problem
-            #message.respond(_("Unknown or incorrectly formed command: %(msg)s... Please re-check your message") % {"msg":message.text[:10]})
-            
+            command_list = [method for method in dir(self) \
+                            if hasattr(getattr(self, method), "format")]
+            input_text = message.text.lower()
+            for command in command_list:
+                format = getattr(self, command).format
+                try:
+                    first_word = (format.split(" "))[0]
+                    if input_text.find(first_word) > -1:
+                        message.respond(format)
+                        return True
+                except:
+                    pass
             return False
         try:
             self.handled = func(self, message, *captures)
         except HandlerFailed, e:
             message.respond(e.message)
-            
+
             self.handled = True
         except Exception, e:
             # TODO: log this exception
@@ -76,7 +122,8 @@ class App (rapidsms.app.App):
         message.was_handled = bool(self.handled)
         return self.handled
 
-    def cleanup (self, message):
+    def cleanup(self, message):
+        ''' log message '''
         if bool(self.handled):
             log = MessageLog(mobile=message.peer,
                          sent_by=message.persistant_connection.reporter,
@@ -84,29 +131,31 @@ class App (rapidsms.app.App):
                          was_handled=message.was_handled)
             log.save()
 
-    def outgoing (self, message):
+    def outgoing(self, message):
         '''Handle outgoing message notifications.'''
         pass
 
-    def stop (self):
+    def stop(self):
         '''Perform global app cleanup when the application is stopped.'''
         pass
 
-    
     @keyword(r'takeover (\w+)')
-    #uncomment this to test multiple decorators- was a test for eliane by ukanga.
+    #uncomment this to test multiple decorators -
+    # was a test for eliane by ukanga.
     #@adeco
     #@registered
     #@anotherdeco
     def reporter_http_identity(self, message, username):
-        '''Hello World! This is a docstring'''
+        '''Gives a http identity to a httptester number of a
+        registered reporter'''
         reporter = self.find_provider(message, username)
         # attach the reporter to the current connection
         if PersistantBackend.from_message(message).title == "http":
             mobile = reporter.connection().identity
             if mobile == message.persistant_connection.identity:
                 message.respond("You already have a http identity!")
-            elif mobile[0] == "+" and mobile[1:] == message.persistant_connection.identity: 
+            elif mobile[0] == "+" \
+                and mobile[1:] == message.persistant_connection.identity:
                 message.persistant_connection.reporter = reporter
                 message.persistant_connection.save()
                 message.respond("You now have a http identity!")
@@ -115,16 +164,17 @@ class App (rapidsms.app.App):
         else:
             message.respond("Operation not allowed under this mode!")
         return True
-   
-    def respond_not_registered (self, message, target):
+    reporter_http_identity.format = "takeover [(@)username]"
+
+    def respond_not_registered(self, message, target):
         raise HandlerFailed(_("User @%s is not registered.") % target)
 
-    def find_provider (self, message, target):
+    def find_provider(self, message, target):
         try:
             if re.match(r'^\d+$', target):
                 reporter = Reporter.objects.get(id=target)
             else:
-                reporter = Reporter.objects.get(alias__iexact=target)                
+                reporter = Reporter.objects.get(alias__iexact=target)
             return reporter
         except models.ObjectDoesNotExist:
             # FIXME: try looking up a group
