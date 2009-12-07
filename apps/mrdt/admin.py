a377d45834672cedbf02320d7319bbef3601d41c
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 08:53:08 +0300

    pep8

--- apps/mrdt/admin.py
+++ apps/mrdt/admin.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from django.contrib import admin
 
 from models import ReportMalaria
 
+
 class ReportMalariaAdmin(admin.ModelAdmin):
-    list_display = ("case","name","location","result", "bednet","entered_at","reporter")
+    list_display = ("case", "name", "location", "result", \
+                    "bednet", "entered_at", "reporter")
     verbose_name = "Malaria Report"
     verbose_name_plural = "Malaria Reports"
 
