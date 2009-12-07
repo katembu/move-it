50b435d97f5f030f15ebd80f3a8adcc61f19205b
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 09:13:45 +0300

    updated docstrings

--- apps/muac/tests.py
+++ apps/muac/tests.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
 from rapidsms.tests.scripted import TestScript
 from app import App
+
 
 class TestApp (TestScript):
     apps = (App,)
