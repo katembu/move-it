4e2481ddbdad19a32cdd186bdbb004023bd9113d
Ukang'a Dickson <ukanga@shiver.(none)>
2009-12-07 09:58:50 +0300

    pep8

--- apps/muac/admin.py
+++ apps/muac/admin.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from django.contrib import admin
 
 from models import ReportMalnutrition
 
+
 class ReportMalnutritionAdmin(admin.ModelAdmin):
-    list_display = ("case", "name","location", "muac", "entered_at","reporter")
-    search_fields = ['case__ref_id' ]
+    list_display = ("case", "name", "location", "muac", "entered_at", \
+                    "reporter")
+    search_fields = ['case__ref_id']
 
 admin.site.register(ReportMalnutrition, ReportMalnutritionAdmin)
-
