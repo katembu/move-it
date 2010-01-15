36576c9cbdb4ceb260e2c28f0ea821d2757ff8d2
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-15 21:28:48 +0300

    pep8 cleanup

--- apps/childcount/app.py
+++ apps/childcount/app.py
@@ -1,27 +1,29 @@
 import rapidsms
 
+
 class App (rapidsms.app.App):
-    def start (self):
+
+    def start(self):
         """Configure your app in the start phase."""
         pass
 
-    def parse (self, message):
+    def parse(self, message):
         """Parse and annotate messages in the parse phase."""
         pass
 
-    def handle (self, message):
+    def handle(self, message):
         """Add your main application logic in the handle phase."""
         pass
 
-    def cleanup (self, message):
+    def cleanup(self, message):
         """Perform any clean up after all handlers have run in the
            cleanup phase."""
         pass
 
-    def outgoing (self, message):
+    def outgoing(self, message):
         """Handle outgoing message notifications."""
         pass
 
-    def stop (self):
+    def stop(self):
         """Perform global app cleanup when the application is stopped."""
         pass
