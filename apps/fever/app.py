8f40ab265431a9c0703749191411d9665d063759
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-18 00:48:04 +0300

    basic setup for a childcount app.py

--- apps/fever/app.py
+++ apps/fever/app.py
@@ -1,6 +1,13 @@
 import rapidsms
+from rapidsms.parsers.keyworder import Keyworder 
 
 from functools import wraps
+
+from childcount.models.config import Configuration as Cfg
+
+
+class HandlerFailed (Exception):
+    pass
 
 
 def registered(func):
@@ -22,7 +29,13 @@ def registered(func):
     return wrapper
 
 
-class App(rapidsms.app.App):
+class App (rapidsms.app.App):
+    """Main ChildCount App
+    """
+
+    MAX_MSG_LEN = 140
+    keyword = Keyworder()
+    handled = False
 
     def start(self):
         """Configure your app in the start phase."""
@@ -33,8 +46,45 @@ class App(rapidsms.app.App):
         pass
 
     def handle(self, message):
-        """Add your main application logic in the handle phase."""
-        pass
+        
+        ''' Function selector
+
+        Matchs functions with keyword using Keyworder
+        Replies formatting advices on error
+        Return False on error and if no function matched '''
+        try:
+            func, captures = self.keyword.match(self, message.text)
+        except TypeError:
+            # didn't find a matching function
+            # make sure we tell them that we got a problem
+            command_list = [method for method in dir(self) \
+                            if hasattr(getattr(self, method), "format")]
+            input_text = message.text.lower()
+            for command in command_list:
+                format = getattr(self, command).format
+                try:
+                    first_word = (format.split(" "))[0]
+                    if input_text.find(first_word) > -1:
+                        message.respond(format)
+                        return True
+                except:
+                    pass
+            return False
+        try:
+            self.handled = func(self, message, *captures)
+        except HandlerFailed, e:
+            message.respond(e.message)
+
+            self.handled = True
+        except Exception, e:
+            # TODO: log this exception
+            # FIXME: also, put the contact number in the config
+            message.respond(_("An error occurred. Please call %(mobile)s") \
+                            % {'mobile': Cfg.get('developer_mobile')})
+
+            raise
+        message.was_handled = bool(self.handled)
+        return self.handled
 
     def cleanup(self, message):
         """Perform any clean up after all handlers have run in the
