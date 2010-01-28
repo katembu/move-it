09d158b5552c145bbecbb0f125f485c35a6bcd24
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-28 06:21:55 +0300

    changed ReportMalaria to Fever report and inherited from Patient Report

--- apps/childcount/fever/models.py
+++ apps/childcount/fever/models.py
@@ -12,11 +12,13 @@ from django.db import models
 from datetime import datetime
 
 from childcount.core.models.Case import Case
-from childcount.core.models.Observation import Observation
+from childcount.core.models.DangerSigns import DangerSigns
+from childcount.core.models.Reports import PatientReport
+
 from reporters.models import Reporter
 
 
-class ReportMalaria(models.Model):
+class FeverReport(PatientReport):
 
     '''records reported malaria rdt tests'''
 
@@ -24,20 +26,9 @@ class ReportMalaria(models.Model):
         get_latest_by = 'entered_at'
         ordering = ('-entered_at',)
         app_label = 'childcount'
-        verbose_name = 'Malaria Report'
-        verbose_name_plural = 'Malaria Reports'
+        verbose_name = 'Fever Report'
+        verbose_name_plural = 'Fever Reports'
 
-    case = models.ForeignKey(Case, db_index=True)
-    reporter = models.ForeignKey(Reporter, db_index=True)
-    entered_at = models.DateTimeField(db_index=True)
-    updated_at = models.DateTimeField(auto_now=True)
     bednet = models.BooleanField(db_index=True)
     result = models.BooleanField(db_index=True)
-    observed = models.ManyToManyField(Observation, blank=True)
-
-    def save(self, *args):
-        if not self.id:
-            self.created_at = datetime.now()
-        else:
-            self.updated_at = datetime.now()
-        super(ReportMalaria, self).save(*args)
+    danger_signs = models.ManyToManyField(DangerSigns, blank=True)
