489dd70bdaca8b95255d21638eba25a7e78c8427
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-28 06:03:51 +0300

    added a PatientReport model

--- apps/childcount/core/models/Reports.py
+++ apps/childcount/core/models/Reports.py
@@ -2,6 +2,14 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 # maintainer: ukanga
 
+'''Report Models
+
+CCReport
+PatientReport
+'''
+
+from datetime import datetime
+
 from django.db import models
 
 from reporters.models import Reporter
@@ -9,12 +17,13 @@ from reporters.models import Reporter
 from childcount.core.models.Patient import Patient
 
 
-class Report(models.Model):
+class CCReport(models.Model):
     '''Report Superclass'''
 
-    reporter = models.ForeignKey(Reporter, db_index=True)
-    reported_on = models.DateTimeField(db_index=True)
-    patient = models.ForeignKey(Patient, db_index=True)
+    created_by = models.ForeignKey(Reporter, db_index=True)
+    created_on = models.DateTimeField(db_index=True)
+    modified_by = models.ForeignKey(Reporter, db_index=True)
+    modified_on = models.DateTimeField(db_index=True)
 
     class Meta:
         app_label = 'childcount'
@@ -22,3 +31,16 @@ class Report(models.Model):
         verbose_name_plural = "ChildCount Reports"
         get_latest_by = 'reported_on'
         ordering = ('-reported_on',)
+
+    def save(self, *args):
+        if not self.id:
+            self.created_on = self.updated_on = datetime.now()
+        else:
+            self.updated_on = datetime.now()
+        super(CCReport, self).save(*args)
+
+
+class PatientReport(CCReport):
+    '''Patient reports'''
+
+    patient = models.ForeignKey(Patient, db_index=True)
