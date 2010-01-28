b7e8e54538dbcf4fcbd2eef0be4c830fe5806d75
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-28 15:46:50 +0300

    removed unncessary save func

--- apps/childcount/core/models/Reports.py
+++ apps/childcount/core/models/Reports.py
@@ -21,9 +21,9 @@ class CCReport(models.Model):
     '''Report Superclass'''
 
     created_by = models.ForeignKey(Reporter, db_index=True)
-    created_on = models.DateTimeField(db_index=True)
+    created_on = models.DateTimeField(db_index=True, auto_now_add=True,)
     modified_by = models.ForeignKey(Reporter, db_index=True)
-    modified_on = models.DateTimeField(db_index=True)
+    modified_on = models.DateTimeField(db_index=True, auto_now=True,)
 
     class Meta:
         app_label = 'childcount'
@@ -31,13 +31,6 @@ class CCReport(models.Model):
         verbose_name_plural = "ChildCount Reports"
         get_latest_by = 'reported_on'
         ordering = ('-reported_on',)
-
-    def save(self, *args):
-        if not self.id:
-            self.created_on = self.updated_on = datetime.now()
-        else:
-            self.updated_on = datetime.now()
-        super(CCReport, self).save(*args)
 
 
 class PatientReport(CCReport):
