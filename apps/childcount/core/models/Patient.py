129138128619280ce54033a7d344c37beef5a95d
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-29 15:18:29 +0300

    added verbose to all fields

--- apps/childcount/core/models/Patient.py
+++ apps/childcount/core/models/Patient.py
@@ -15,10 +15,13 @@ from datetime import datetime
 from reporters.models import Reporter
 from locations.models import Location
 
+from childcount.core.models.fields import GenderField
+from childcount.core.models.locations import Clinic, Zone
+
 from childcount.core.models.CHW import CHW
 
 
-class Patient(models.Model):
+class Patient(GenderField):
 
     '''Holds the patient details, properties and methods related to it'''
 
@@ -38,25 +41,46 @@ class Patient(models.Model):
         ('M', _('Male')),
         ('F', _('Female')))
 
-    health_id = models.CharField(_('Patient ID #'), null=True, \
-                                  db_index=True, max_length=10,
-                                  unique=True)
-    first_name = models.CharField(max_length=255, db_index=True)
-    middle_name =  models.CharField(max_length=255, db_index=True, \
-                                    null=True, blank=True)
-    last_name = models.CharField(max_length=255, db_index=True)
+    health_id = models.CharField(_(u"Health ID"), max_length=6, blank=True, \
+                                null=True, db_index=True, unique=True, \
+                                help_text=_(u"Unique six character Health ID"))
+    first_name = models.CharField(_(u"First name"), max_length=32)
+    middle_name =  models.CharField(_(u"Middle name"), max_length=32, \
+                                   blank=True, null=True)
+    last_name = models.CharField(_(u"Last name"), max_length=32, \
+                                 help_text=_(u"Family name or surname"))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, \
                               null=True, blank=True)
     dob = models.DateField(_('Date of Birth'), null=True, blank=True)
-    estimated_dob = models.NullBooleanField(null=True, blank=True)
-    guardian = models.ForeignKey(Reporter, db_index=True)
-    household = models.ForeignKey(Reporter, db_index=True)
-    chw = models.ForeignKey(CHW, db_index=True)
-    zone = models.ForeignKey(Location, db_index=True)
-    clinic = models.ForeignKey(Location, db_index=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+    estimated_dob = models.BooleanField(_(u"Estimated DOB"), \
+                                        help_text=_(u"True or false: the " \
+                                                     "date of birth is only " \
+                                                     "an approximation"))
+    guardian = models.ForeignKey('self', blank=True, null=True, \
+                                 verbose_name=_(u"Mother or guardian"), \
+                                 related_name='child')
+    household = models.ForeignKey('self', \
+                                  verbose_name=_(u"Household's primary " \
+                                                 "caregiver"), \
+                                  help_text=_(u"The primary caregiver in " \
+                                              "this person's household " \
+                                              "(self if primary caregiver)"), \
+                                  related_name='household_member')
+    chw = models.ForeignKey(CHW, db_index=True,
+                            verbose_name=_(u"Community health worker"))
+    zone = models.ForeignKey(Zone, blank=True, null=True,
+                             verbose_name=_(u"Location"), \
+                             help_text=_(u"The location this person lives " \
+                                          "within"))
+    clinic = models.ForeignKey(Clinic, blank=True, null=True, \
+                               verbose_name=_(u"Health facility"), \
+                               help_text=_(u"The primary health facility " \
+                                            "that this patient visits"))
+    created_on = models.DateTimeField(_(u"Created on"), auto_now_add=True, \
+                                      help_text=_(u"When the patient record " \
+                                                   "was created"))
     updated_on = models.DateTimeField(auto_now=True)
-    mobile =  models.CharField(max_length=255, db_index=True, \
-                                    null=True, blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, \
+    mobile = models.CharField(_(u"Mobile phone number"), max_length=16, \
+                              blank=True, null=True)
+    status = models.SmallIntegerField(_(u"Status"), choices=STATUS_CHOICES, \
                                       default=STATUS_ACTIVE)
