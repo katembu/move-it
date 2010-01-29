6b5dd54fce716d0c23b7a12e17bfbeb6162d568b
Ukang'a Dickson <ukanga@shiver.(none)>
2010-01-29 15:26:26 +0300

    added RDTField abstract class

--- apps/childcount/core/models/fields.py
+++ apps/childcount/core/models/fields.py
@@ -20,3 +20,27 @@ class GenderField(models.Model):
 
     gender = models.CharField(_(u"Gender"), max_length=1, \
                               choices=GENDER_CHOICES)    
+
+
+class RDTField(models.Model):
+    '''
+    This is an abstract class that includes an RDT field.  It does not
+    have a table in the database, and is only used by extending it by other
+    model classes. Any class that extends it will have an rdt_result field.
+    '''
+    class Meta:
+        abstract = True
+
+    RDT_POSITIVE = 'P'
+    RDT_NEGATIVE = 'N'
+    RDT_UNKOWN = 'U'
+    RDT_UNAVAILABLE = 'X'
+
+    RDT_CHOICES = (
+        (RDT_POSITIVE, _(u"Positive")),
+        (RDT_NEGATIVE, _(u"Negative")),
+        (RDT_UNKOWN, _(u"Unknown")),
+        (RDT_UNAVAILABLE, _(u"Test unavailable")))
+
+    rdt_result = models.CharField(_(u"RDT Result"), max_length=1, \
+                                  choices=RDT_CHOICES)
