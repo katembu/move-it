e5e05a80a00aa671036d7ef25bbb9a793945c995
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-01 00:40:32 +0300

    added logic for new_born_section

---
+++ apps/childcount/child/logic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+'''Child logic
+'''
+
+from django.utils.translation import ugettext_lazy as _
+
+from childcount.core.models.Patient import Patient
+from childcount.core.models.Referral import Referral
+
+  
+def new_born_section(health_id, danger_sign, visits):
+    '''1.1) Newborn Section (0-30 days) (REQUIRED)'''
+
+    patient = Patient.objects.get(health_id=health_id)
+    days, months = patient.age_in_days_months()
+    response = ''
+
+    if days > 30 and months < 6:
+        response = _('Child is no longer a newborn. Please fill out INFANT ' \
+                     '(+I) form')
+    elif months > 6 and months < 60:
+        response = _('Child is %(months)d months old. Please fill out CHILD ' \
+                     '(+C) form') % {'months': months}
+    elif months > 59:
+        response = _('Child is older then 59 months.')
+    else:
+        if danger_sign.upper() == 'Y':
+            #create a referral
+            rf = Referral(patient=patient)
+            rf.save()
+            response = _('Please refer child immediately to clinic '\
+                         '(#%(refid)s)') % {'refid': rf.referral_id}
+        else:
+            response = _('Clinic visit %(visits)s registered for newborn.') % \
+                        {'visits': visits}
+
+    return response
+    
