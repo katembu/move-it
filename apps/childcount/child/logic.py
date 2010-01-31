eff47fd185edce05377bfcbf3e6135ceba09f319
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-01 01:31:06 +0300

    save newborn report

--- apps/childcount/child/logic.py
+++ apps/childcount/child/logic.py
@@ -9,9 +9,9 @@ from django.utils.translation import ugettext_lazy as _
 
 from childcount.core.models.Patient import Patient
 from childcount.core.models.Referral import Referral
-
+from childcount.core.models.Reports import NewbornReport
   
-def new_born_section(health_id, danger_sign, visits):
+def new_born_section(created_by, health_id, danger_signs, visits):
     '''1.1) Newborn Section (0-30 days) (REQUIRED)'''
 
     patient = Patient.objects.get(health_id=health_id)
@@ -27,7 +27,7 @@ def new_born_section(health_id, danger_sign, visits):
     elif months > 59:
         response = _('Child is older then 59 months.')
     else:
-        if danger_sign.upper() == 'Y':
+        if danger_signs.upper() == 'Y':
             #create a referral
             rf = Referral(patient=patient)
             rf.save()
@@ -36,6 +36,9 @@ def new_born_section(health_id, danger_sign, visits):
         else:
             response = _('Clinic visit %(visits)s registered for newborn.') % \
                         {'visits': visits}
+        nr = NewbornReport(created_by=created_by, patient=patient, \
+                           danger_signs=danger_signs)
+        nr.save()
 
     return response
     
