9e828debcc27e861d47435ae69f74cde102aa0ca
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-07 23:22:19 +0300

    death_section for patients with health id

--- apps/childcount/core/logic.py
+++ apps/childcount/core/logic.py
@@ -14,6 +14,7 @@ from django.utils.translation import ugettext_lazy as _
 from childcount.core.models.Patient import Patient
 from childcount.core.models.Reports import HouseHoldVisitReport
 from childcount.core.models.Reports import PregnancyReport
+from childcount.core.models.Reports import DeathReport
 from childcount.core.models.Case import Case
 from childcount.child.models import BirthReport
 
@@ -383,3 +384,48 @@ def birth_section(created_by, health_id, token_string):
         response = _("Birth %(health_id)s: %(last_name)s, %(first_name)s " \
                     "%(gender)s/%(age)s (%(guardian)s) %(location)s") % info
     return response
+
+
+def death_section(created_by, health_id, dod):
+    patient = Patient.objects.get(health_id=health_id)
+
+    dod_str = dod
+    dod = re.sub(r'\D', '', dod)
+    years_months = dod_str.replace(dob, '')
+    if len(dod) >= 3:
+        try:
+            # TODO this 2 step conversion is too complex, simplify!
+            dod = time.strptime(dod, "%d%m%y")
+            dod = date(*dod[:3])
+        except ValueError:
+            try:
+                # TODO this 2 step conversion is too complex, simplify!
+                dod = time.strptime(dod, "%d%m%Y")
+                dod = date(*dod[:3])
+            except ValueError:
+                raise HandlerFailed(_("Couldn't understand date: %(dod)s")\
+                                    % {'dod': dod})
+    # if there are fewer than three digits, we are
+    # probably dealing with an age (in months),
+    # so attempt to estimate a dod
+    else:
+        # TODO move to a utils file? (almost same code in import_cases.py)
+        try:
+            if dod.isdigit():
+                if years_months.upper() == 'Y':
+                    dod = int(dod) * 12
+                years = int(dod) / 12
+                months = int(dod) % 12
+                est_year = abs(date.today().year - int(years))
+                est_month = abs(date.today().month - int(months))
+                if est_month == 0:
+                    est_month = 1
+                estimate = ("%s-%s-%s" % (est_year, est_month, 15))
+                # TODO this 2 step conversion is too complex, simplify!
+                dod = time.strptime(estimate, "%Y-%m-%d")
+                dod = date(*dod[:3])
+
+        except Exception:
+            pass
+    dr = DeathReport(created_by=created_by, patient=patient, dod=dod)
+    dr.save()
