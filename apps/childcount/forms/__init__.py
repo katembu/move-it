54fb41622825169ab19d9fff9df0f71e89ba9e67
David Gelvin <david.gelvin@gmail.com>
2010-02-10 10:52:23 +0300

    Fever form

--- apps/childcount/forms/__init__.py
+++ apps/childcount/forms/__init__.py
@@ -2,5 +2,6 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 # maintainer: dgelvin
 
-from CCForm import CCForm
-from MUACForm import MUACForm
+from childcount.forms.CCForm import CCForm
+from childcount.forms.MUACForm import MUACForm
+from childcount.forms.FeverForm import FeverForm
