c099caf4be1cd5c93a7d5dc765231b269ac65546
David Gelvin <david.gelvin@gmail.com>
2010-02-10 16:48:53 +0300

    Registered models with django admin

--- apps/childcount/admin.py
+++ apps/childcount/admin.py
@@ -4,12 +4,22 @@
 
 from django.contrib import admin
 
-from childcount.models import Configuration
-from childcount.models import Case
-from childcount.models import Patient
-from childcount.models import Referral
+from childcount.models import *
 
 admin.site.register(Configuration)
 admin.site.register(Case)
 admin.site.register(Patient)
 admin.site.register(Referral)
+admin.site.register(CHW)
+admin.site.register(Clinic)
+admin.site.register(Commodity)
+admin.site.register(DangerSign)
+admin.site.register(DangerSignTranslation)
+admin.site.register(NonPatientRDTReport)
+admin.site.register(HealthReport)
+admin.site.register(DeathReport)
+admin.site.register(FeverReport)
+admin.site.register(PatientRegistrationReport)
+admin.site.register(BirthReport)
+admin.site.register(NewbornReport)
+
