571f7605cad5a3a4cbad70d441ca10a1ea1905a2
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-11 12:04:21 +0300

    updated childform

--- apps/childcount/app.py
+++ apps/childcount/app.py
@@ -26,7 +26,7 @@ class App (rapidsms.app.App):
     DEFAULT_LANGUAGE = 'en'
     
     COMMANDS = [RegistrationCommand, WhoCommand]
-    FORMS = [MUACForm, HealthStatusForm, FeverForm, MobileForm, NewbornForm]
+    FORMS = [MUACForm, HealthStatusForm, FeverForm, MobileForm, NewbornForm, CHildForm]
     MATCH_ALL_LANG_CHAR = '*'
     
     command_keywords = {}
