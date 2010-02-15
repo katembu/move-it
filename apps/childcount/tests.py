27c91969d84f8a724ed099f2c697075adc411738
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-15 10:36:37 +0300

    updated childcount unittests

--- apps/childcount/tests.py
+++ apps/childcount/tests.py
@@ -1,21 +1,62 @@
+import os
+
 from rapidsms.tests.scripted import TestScript
+from django.core.management.commands.loaddata import Command
 
 import reporters.app as reporter_app
+import locations.app as locations_app
+
+from locations.models import *
+from childcount.models import CHW
 
 from app import App
-from childcount.forms import ChildForm
-from childcount.commands import WhoCommand
+from childcount.forms import *
+from childcount.commands import *
 
 
 class TestApp (TestScript):
-    apps = (reporter_app.App, App,)
-    App.FORMS = [ChildForm]
-    App.COMMANDS = [WhoCommand]
+    apps = (reporter_app.App, locations_app.App, App,)
+    App.FORMS = [PatientRegistrationForm, MUACForm, HealthStatusForm, FeverForm, MobileForm, NewbornForm, ChildForm, HouseHoldVisitForm, PregnancyForm, PostpartumForm, ReferralForm, DeathForm, DispensationForm]
+    App.COMMANDS = [WhoCommand, RegistrationCommand]
+    fixtures = ['uganda_locations', 'uganda_locations_type']
 
-    testWho = """
-        123 > who
-        123 < 123 is not a registered number.
-    """
+    def testAll(self):
+        #print Location.objects.all()
+        self.assertEquals(Location.objects.all().count(), 0)
+        ldc = Command()
+        filename = os.path.abspath(os.path.join(os.path.dirname(__file__),"fixtures/uganda_locations_type"))
+        ldc.handle('location', filename)
+        filename = os.path.abspath(os.path.join(os.path.dirname(__file__),"fixtures/uganda_locations"))
+        ldc.handle('location', filename)
+        #print Location.objects.all()
+        self.assertEquals(Location.objects.all().count(), 98)
+        
+        testRegister = """
+            123 > who
+            123 < 123 is not a registered number.
+            123 > chw bidxcvxxx lname fname
+            123 < Location bidxcvxxx does not exist
+            123 > chw bir lname fname
+            123 < Success. You are now registered at Birare with alias @flname.
+        """
+        self.runScript(testRegister)
+
+        chw = CHW.objects.get(alias='flname')
+        self.assertEqual(chw.first_name, 'Fname')
+
+        testWho = """
+            123 > who
+            123 < You are registered as Fname Lname from Birare with alias @flname.
+        """
+        self.runScript(testWho)
+
+        testForms = """
+            123 > d47q +a 2 +r c
+            123 < D47Q is not a valid health ID. Please correct and send forms for that patient again.
+            123 > d47q +new jane mini f 110984 p +mob 0727944684
+            123 < D47Q: You successfuly registered D46Q Jane Mini F/25y +MOB[0727944684]
+        """
+        self.runScript(testForms)
 
     # define your test scripts here.
     # e.g.:
