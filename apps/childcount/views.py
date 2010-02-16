86678584e88ab4036dc535d7f62de684f30e0262
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-16 09:18:23 +0300

    Added a web patient listing with client side search: jquery Datatable

--- apps/childcount/views.py
+++ apps/childcount/views.py
@@ -3,6 +3,9 @@
 # maintainer: ukanga
 
 from rapidsms.webui.utils import render_to_response
+from django.utils.translation import gettext_lazy as _
+
+from childcount.models import Patient
 
 
 def index(request):
@@ -11,3 +14,55 @@ def index(request):
     title = "ChildCount-2.0"
     return render_to_response(request, template_name, {
             "title": title})
+
+def patient(request):
+    '''Patients page '''
+    report_title = Patient._meta.verbose_name
+    rows = []
+
+    reports = Patient.objects.all()
+    i = 0
+    for report in reports:
+        i += 1
+        row = {}
+        row["cells"] = []
+        row["cells"].append({"value": report.first_name})
+        row["cells"].append({"value": report.last_name})
+        row["cells"].append({"value": report.chw})
+
+        if i == 100:
+            row['complete'] = True
+            rows.append(row)
+            break
+        rows.append(row)
+
+    columns, sub_columns = Patient.table_columns()
+
+    aocolumns_js = "{ \"sType\": \"html\" },"
+    for col in columns[1:] + (sub_columns if sub_columns != None else []):
+        if not 'colspan' in col:
+            aocolumns_js += "{ \"asSorting\": [ \"desc\", \"asc\" ], " \
+                            "\"bSearchable\": false },"
+    aocolumns_js = aocolumns_js[:-1]
+
+    aggregate = False
+    print columns
+    print sub_columns
+    print len(rows)
+    context_dict = {'get_vars': request.META['QUERY_STRING'],
+                    'columns': columns, 'sub_columns': sub_columns,
+                    'rows': rows, 'report_title': report_title,
+                    'aggregate': aggregate, 'aocolumns_js': aocolumns_js}
+
+    if request.method == 'GET' and 'excel' in request.GET:
+        '''response = HttpResponse(mimetype="application/vnd.ms-excel")
+        filename = "%s %s.xls" % \
+                   (report_title, datetime.now().strftime("%d%m%Y"))
+        response['Content-Disposition'] = "attachment; " \
+                                          "filename=\"%s\"" % filename
+        from findug.utils import create_excel
+        response.write(create_excel(context_dict))
+        return response'''
+        return render_to_response(request, 'childcount/patient.html', context_dict)
+    else:
+        return render_to_response(request, 'childcount/patient.html', context_dict)
