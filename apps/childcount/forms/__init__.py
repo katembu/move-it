c14ed8d1f468a29a0111baf4e78dc99f1c018932
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-16 18:06:35 +0300

    added NeonatalForm

--- apps/childcount/forms/__init__.py
+++ apps/childcount/forms/__init__.py
@@ -17,5 +17,6 @@ from childcount.forms.PostpartumForm import PostpartumForm
 from childcount.forms.ReferralForm import ReferralForm
 from childcount.forms.DispensationForm import DispensationForm
 from childcount.forms.DiarrheaForm import DiarrheaForm
+from childcount.forms.ChildForm import NeonatalForm
 
 import utils
