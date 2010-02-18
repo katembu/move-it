ca6435615aaaaceaf8bbba927eb58a57ff39932a
Eliane Stampfer <eliane@eliane-ubuntu9.10>
2010-02-18 13:49:33 +0000

    very basic CHW report

--- apps/childcount/urls.py
+++ apps/childcount/urls.py
@@ -22,5 +22,6 @@ urlpatterns = patterns('',
     url(r'^static/childcount/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': 'apps/childcount/static', 'show_indexes': True}),
     url(r'^childcount/?$', views.index),
-    url(r'^childcount/patients/?$', views.patient)
+    url(r'^childcount/patients/?$', views.patient),
+    url(r'^childcount/chw/?$', views.chw)
 )
