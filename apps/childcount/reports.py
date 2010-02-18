7719101778c50360e4303b283dc20357166f988f
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-18 20:03:09 +0300

    added patients per chw pdf report

--- apps/childcount/reports.py
+++ apps/childcount/reports.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 from django.template import Template, Context
 
 from childcount.models import Patient
+from childcount.models import CHW
 from childcount.models.ccreports import ThePatient
 
 from libreport.pdfreport import PDFReport
@@ -22,10 +23,32 @@ def all_patient_list_pdf(request):
 
     for report in reports:
         rows.append([data for data in cols])
-        pass
 
     rpt = PDFReport()
     rpt.setTitle(report_title )
     rpt.setFilename(report_title + '.pdf')
     rpt.setTableData(reports, cols, _("All Patients"))
+    return rpt.render()
+
+
+def all_patient_list_per_chw_pdf(request):
+    report_title = Patient._meta.verbose_name
+
+    rpt = PDFReport()
+    rpt.setTitle(report_title )
+    rpt.setFilename(report_title + '.pdf')
+
+    cols, sub_cols = ThePatient.patients_summary_list()
+
+    chws = CHW.objects.all()
+    for chw in chws:
+        rows = []
+        reports = ThePatient.objects.filter(chw=chw).order_by('household')
+
+        for report in reports:
+            rows.append([data for data in cols])
+
+        rpt.setTableData(reports, cols, chw)
+        rpt.setPageBreak()
+
     return rpt.render()
