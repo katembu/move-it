a6d06d5298023ef402495637f7306d16ddb97882
Ukang'a Dickson <ukanga@shiver.(none)>
2010-02-25 15:56:18 +0300

    underfive reports modified

--- apps/childcount/urls.py
+++ apps/childcount/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
         {'document_root': 'apps/childcount/static', 'show_indexes': True}),
     url(r'^childcount/?$', views.index),
     url(r'^childcount/patients/?$', views.patient),
-    url(r'^childcount/patients/pdf/?$', reports.all_patient_list_pdf),
+    url(r'^childcount/patients/(?P<rfilter>[a-z]+)/(?P<rformat>[a-z]+)?$', reports.all_patient_list_pdf),
     url(r'^childcount/patients_per_chw/pdf/?$', \
                                     reports.all_patient_list_per_chw_pdf),
     url(r'^childcount/chw/?$', views.chw),
