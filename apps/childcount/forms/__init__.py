ac63808e0f3e4e664e6c336a3dde6468857cadd8
Ukang'a Dickson <ukanga@gmail.com>
2010-05-21 02:01:50 +0300

    added SauriUnderOneForm: it contains the extra field for vaccinations given

--- apps/childcount/forms/__init__.py
+++ apps/childcount/forms/__init__.py
@@ -27,3 +27,7 @@ from childcount.forms.SickMembersForm import SickMembersForm
 from childcount.forms.VerbalAutopsyForm import VerbalAutopsyForm
 
 from childcount.forms import utils
+
+#sauri specific
+from childcount.forms.SauriUnderOneForm import SauriUnderOneForm
+#endsauri
