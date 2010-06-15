48038e032b1a1facad7bef065e99f290e8dc5b97
katembu <mose.kate@gmail.com>
2010-06-15 18:31:55 +0300

    Generate chart

--- apps/childcount/static/scripts/example.js
+++ apps/childcount/static/scripts/example.js
@@ -1,8 +1,8 @@
 // Run the script on DOM ready:
 $(function(){
  	$('#moses').visualize({type: 'pie', height: '300px', width: '420px'});
-	$('#moses').visualize({type: 'bar', width: '420px'});
-	$('#moses').visualize({type: 'area', width: '420px'});
-	$('#moses').visualize({type: 'line', width: '420px'});
+	$('#moses').visualize({type: 'bar', width: '600px'});
+	//$('#moses').visualize({type: 'area', width: '420px'});
+	//$('#moses').visualize({type: 'line', width: '420px'});
 
 });
