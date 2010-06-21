c84a346788c0fd94f74bcc0ae3d7fb2163123cd5
Ukang'a Dickson <ukanga@gmail.com>
2010-06-21 11:26:41 +0300

    reworked operations report

--- apps/childcount/reports.py
+++ apps/childcount/reports.py
@@ -2,22 +2,41 @@
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 # maintainer: ukanga
 
+import os
+import copy
+
 from rapidsms.webui.utils import render_to_response
 from django.utils.translation import gettext_lazy as _
 from django.template import Template, Context
+from django.http import HttpResponse
 
 try:
+    from reportlab.lib.styles import getSampleStyleSheet
+    from reportlab.lib.pagesizes import letter, landscape, A4
+    from reportlab.platypus import Paragraph, SimpleDocTemplate, PageBreak
+    from reportlab.platypus import Table, TableStyle
+    from reportlab.lib import colors
     from reportlab.lib.units import inch
+    from reportlab.lib.enums import TA_CENTER
 except ImportError:
     pass
 
+from childcount.models import Clinic
 from childcount.models.ccreports import TheCHWReport
 from childcount.models.ccreports import ThePatient, OperationalReport
+from childcount.utils import RotatedParagraph
 
 from libreport.pdfreport import PDFReport, p
 from libreport.csvreport import CSVReport
 
 from locations.models import Location
+
+styles = getSampleStyleSheet()
+
+styleN = styles['Normal']
+styleH = styles['Heading1']
+styleH3 = styles['Heading3']
+styleH4 = styles['Heading4']
 
 
 def all_patient_list_pdf(request, rfilter=u'all', rformat="html"):
@@ -196,27 +215,104 @@ def chw(request, rformat='html'):
 
 
 def operationalreport(request, rformat):
-    opr = OperationalReport()
-    if rformat == u'csv':
-        rpt = CSVReport()
-        rpt.setTableData(TheCHWReport.objects.all(), opr.get_columns(), \
-            'Operational Report')
-    else:
-        #pdf
-        rpt = PDFReport()
-        rpt.setLandscape(False)
-        rpt.setFirstRowHeight(2.5)
-        rpt.rotateText(True)
-        rpt.setRowsPerPage(19)
-        colWidths = [1 * inch]
-        colWidths.extend((len(opr.get_columns()) - 1) * [0.4 * inch] )
-        for location in Location.objects.all():
-            if not TheCHWReport.objects.filter(location=location).count():
-                continue
-            rpt.setTableData(TheCHWReport.objects.filter(location=location), \
-                            opr.get_columns(), _('Operational Report %s') % \
-                            location, colWidths=colWidths)
-            rpt.setPageBreak()
+    filename = 'operationalreport.pdf'
+    story = []
+    
+    for location in Clinic.objects.all():
+        if not TheCHWReport.objects.filter(location=location).count():
+            continue
+        tb = operationalreportable(location, TheCHWReport.objects.\
+            filter(location=location))
+        story.append(tb)
+        story.append(PageBreak())
 
-    return rpt.render()
+    doc = SimpleDocTemplate(filename, pagesize = landscape(A4), \
+                            topMargin=(0 * inch), \
+                            bottomMargin=(0 * inch))
+    doc.build(story)
+    response = HttpResponse(mimetype='application/pdf')
+    response['Cache-Control'] = ""
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    response.write(open(filename).read())
+    os.remove(filename)
+    return response
+
+
+def operationalreportable(title, indata=None):
+    styleH3.fontName = 'Times-Bold'
+    styleH3.alignment = TA_CENTER
+    #styleH4
+    styleN2 = copy.copy(styleN)
+    styleN2.alignment = TA_CENTER
+
+    opr = OperationalReport()
+    cols = opr.get_columns()
+
+    hdata = [Paragraph('%s' % title, styleH3)]
+    hdata.extend((len(cols) - 1) * [''])
+    data = [hdata, ['', Paragraph('Household', styleH3), '', '', \
+            Paragraph('Newborn', styleH3), '', '', \
+            Paragraph('Under-5\'s', styleH3), '', \
+            '', '', '', '', Paragraph('Pregnant', styleH3), '', '', \
+            Paragraph('Follow-up', styleH3), '', \
+            Paragraph('SMS', styleH3), ''], \
+            ['', Paragraph('A1', styleH3), Paragraph('A2', styleH3), \
+            Paragraph('A3', styleH3), Paragraph('B1', styleH3), \
+            Paragraph('B2', styleH3), Paragraph('B3', styleH3), \
+            Paragraph('C1', styleH3), Paragraph('C2', styleH3), \
+            Paragraph('C3', styleH3), Paragraph('C4',  styleH3), \
+            Paragraph('C5', styleH3), Paragraph('C6', styleH3), \
+            Paragraph('D1', styleH3), Paragraph('D2', styleH3), \
+            Paragraph('D3', styleH3), Paragraph('E1', styleH3), \
+            Paragraph('E2', styleH3), Paragraph('F1', styleH3), \
+            Paragraph('F2', styleH3)]]
+
+    #styleN.borderWidth = 0
+    #styleN.borderColor = colors.red 
+    thirdrow = [Paragraph(cols[0]['name'], styleH3)]
+    thirdrow.extend([RotatedParagraph(Paragraph(col['name'], styleN), 2.3 * inch, \
+                                    0.25 * inch) for col in cols[1:]])
+    data.append(thirdrow)
+
+    fourthrow = [Paragraph('Target:', styleH3)]
+    fourthrow.extend([Paragraph(item, styleN) for item in ['-', '-', '100', \
+                        '-', '100', '100', '-', '-', '-', '-', '100', '-', \
+                        '-', '-', '100', '100', '<=2', '0', '-']])
+    data.append(fourthrow)
+    
+    fifthrow = [Paragraph('<ul>List of CHWs</ul>', styleH4)]
+    fifthrow.extend([Paragraph(item, styleN) for item in [''] * 19])
+    data.append(fifthrow)
+
+    rowHeights = [None, None, None, 2.3 * inch, 0.25 * inch, 0.25 * inch]
+    colWidths = [1.5 * inch]
+    colWidths.extend((len(cols) - 1) * [0.5 * inch])
+    
+    if indata:
+        for row in indata:
+            ctx = Context({"object": row})
+            values = [Paragraph(Template(col["bit"]).render(ctx), \
+                                styleN) for col in cols]
+            data.append(values)
+        rowHeights.extend(len(indata) * [0.25 * inch])
+    tb = Table(data, colWidths=colWidths, rowHeights=rowHeights, repeatRows=6)
+    tb.setStyle(TableStyle([('SPAN', (0, 0), (19, 0)),
+                            ('INNERGRID', (0, 0), (-1, -1), 0.1, \
+                            colors.lightgrey),\
+                            ('BOX', (0, 0), (-1, -1), 0.1, \
+                            colors.lightgrey), \
+                            ('BOX', (1, 1), (3, -1), 5, \
+                            colors.lightgrey),\
+                            ('SPAN', (1, 1), (3, 1)), \
+                            ('SPAN', (4, 1), (6, 1)), \
+                            ('BOX', (7, 1), (12, -1), 5, \
+                            colors.lightgrey),\
+                            ('SPAN', (7, 1), (12, 1)), \
+                            ('SPAN', (13, 1), (15, 1)), \
+                            ('BOX', (16, 1), (17, -1), 5, \
+                            colors.lightgrey),\
+                            ('SPAN', (16, 1), (17, 1)), \
+                            ('SPAN', (-2, 1), (-1, 1)), \
+                ]))
+    return tb
 
