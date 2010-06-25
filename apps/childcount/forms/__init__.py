167e22b62d988442e5e92619c33ad7e97c5b6bbf
katembu <mose.kate@gmail.com>
2010-06-25 10:54:24 +0300

    bednet issued/distributed model

--- apps/childcount/forms/__init__.py
+++ apps/childcount/forms/__init__.py
@@ -39,3 +39,4 @@ from childcount.forms.BednetCoverageForm import BednetCoverageForm
 from childcount.forms.BednetUtilizationForm import BednetUtilizationForm
 from childcount.forms.SanitationForm import SanitationForm
 from childcount.forms.DrinkingWaterForm import DrinkingWaterForm
+from childcount.forms.BednetDistributionForm import BednetDistributionForm
