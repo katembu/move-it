d411e9a8fff48311a318b69b333d067825299fe4
katembu <mose.kate@gmail.com>
2010-06-29 17:12:25 +0300

    added clinic report

--- apps/childcount/models/ccreports.py
+++ apps/childcount/models/ccreports.py
@@ -9,7 +9,7 @@ from datetime import date, timedelta, datetime
 
 from childcount.models import Patient
 from locations.models import Location
-from childcount.models import CHW
+from childcount.models import CHW, Clinic
 from childcount.models import NutritionReport, FeverReport, ReferralReport
 from childcount.models import BirthReport, PregnancyReport
 from childcount.models import HouseholdVisitReport, FollowUpReport
@@ -479,9 +479,8 @@ class TheCHWReport(CHW):
         return data
 
 
-#display report of each village activeness for the last 28 days(registered)
 class LocationReport(Patient, Location):
-    
+    #display report of each village activeness for the last 28 days(registered)
     @classmethod
     def patients_per_loc(cls):
         #last 28 days
@@ -565,4 +564,56 @@ class OperationalReport():
 
     def get_columns(self):
         return self.columns
+
+class ClinicReport(Clinic):
+
+    class Meta:
+        verbose_name = _("Clinic Report")
+        proxy = True
+   
+    @property
+    def registeredpatient(self):
+        total = Patient.objects.filter(chw__location=self)
+        return total.count()
+
+    @property
+    def househoulds(self):
+        thousehold = Patient.objects.filter(health_id=F(\
+                                                    'household__health_id'),\
+                                        chw__location=self)
+        return thousehold.count()
+
+    @property
+    def pntied_household(self):
+        nttied_household = Patient.objects.filter(chw__location=self,\
+                                            household__health_id='xxxxx')
+        return nttied_household.count()
+
+    @property
+    def rdtreported(self):
+        fr = FeverReport.objects.filter(encounter__patient__chw__location=self)
+        return fr.count()
+    
+
+    @classmethod
+    def summary(cls):
+        columns = []
+        columns.append(
+            {'name': cls._meta.get_field('name').verbose_name.upper(), \
+             'bit': '{{object.name}}'})
+        columns.append(
+            {'name': _("#House hold".upper()), \
+             'bit': '{{object.househoulds}}'})
+        columns.append(
+            {'name': _("#Patients".upper()), \
+             'bit': '{{object.registeredpatient}}'})
+        columns.append(
+            {'name':  _("#Patients not to tied household".upper()), \
+             'bit': '{{object.pntied_household}}'})
+        columns.append(
+            {'name': "#RDT ".upper(), \
+             'bit': '{{object.rdtreported}}'})
+
+        sub_columns = None
+        return columns, sub_columns        
 
