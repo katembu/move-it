24ca24d822307299bbff936976828b94a4ba52be
katembu <mose.kate@gmail.com>
2010-07-05 13:37:32 +0300

    removed example.js

--- apps/childcount/static/scripts/example.js
+++
@@ -1,9 +0,0 @@
-// Run the script on DOM ready:
-$(function(){
-
-	// clinic pie chart
-	//$('#clinicpie').visualize({type: 'pie', height: '270px', width: '350px'});
-	$('#clinicpie').visualize({type: 'bar', width: '600px'});
-	$('#clinicpie').hide()
-
-});
