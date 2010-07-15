7a8c7d8f21fb666aff978d8e3a5e196b39dc7e46
katembu <mose.kate@gmail.com>
2010-07-15 10:23:04 +0300

    added immunization notification, schedule

--- apps/childcount/admin.py
+++ apps/childcount/admin.py
@@ -44,9 +44,13 @@ admin.site.register(BedNetReport, VersionAdmin)
 admin.site.register(SickMembersReport, VersionAdmin)
 admin.site.register(DangerSignsReport, VersionAdmin)
 admin.site.register(MedicineGivenReport, VersionAdmin)
+#Bednet Sanitation
 admin.site.register(BednetUtilization, VersionAdmin)
 admin.site.register(SanitationReport, VersionAdmin)
 admin.site.register(DrinkingWaterReport, VersionAdmin)
+#Immunization
+admin.site.register(ImmunizationSchedule, VersionAdmin)
+admin.site.register(ImmunizationNotification, VersionAdmin)
 
 class CodedItemAdmin(admin.ModelAdmin):
     list_filter = ('type', )
