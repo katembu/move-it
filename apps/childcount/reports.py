16841665895abbb54053658a8a6d0328ea2a574b
Ukang'a Dickson <ukanga@gmail.com>
2010-09-04 03:31:03 +0300

    reverse 76bf55c5e42fc5f3580ec3fbafc4832227879891: should be handled differently

--- apps/childcount/reports.py
+++ apps/childcount/reports.py
@@ -402,8 +402,7 @@ def gen_patient_register_pdf(filename, location):
         for household in households:
             trow = len(patients)
             patients.append(household)
-            hs = ThePatient.objects.filter(household=household, \
-                                            status=ThePatient.STATUS_ACTIVE)\
+            hs = ThePatient.objects.filter(household=household)\
                             .exclude(health_id=household.health_id)\
                             .order_by('household')
             patients.extend(hs)
@@ -417,8 +416,7 @@ def gen_patient_register_pdf(filename, location):
             dh = ThePatient.objects.get(health_id='XXXXX')
             patients.append(dh)
             hs = ThePatient.objects.filter(household=dh, \
-                                            chw=chw, \
-                                            status=ThePatient.STATUS_ACTIVE)\
+                                            chw=chw)\
                                     .exclude(health_id=dh.health_id)
             patients.extend(hs)
             brow = len(patients) - 1
