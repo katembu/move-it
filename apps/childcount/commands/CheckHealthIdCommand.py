e1de56a045bac3709f058dbea99f8c132f001eb3
Henry C-G <noemail@noemail>
2010-09-14 14:54:55 +0300

    More work on checkid command

--- apps/childcount/commands/CheckHealthIdCommand.py
+++ apps/childcount/commands/CheckHealthIdCommand.py
@@ -6,6 +6,8 @@ import re
 
 from django.utils.translation import ugettext as _
 
+from checksum import checksum
+
 from reporters.models import Reporter
 from locations.models import Location
 from childcount.models import HealthId
@@ -33,7 +35,7 @@ class CheckHealthIdCommand(CCCommand):
         health_id = self.params[1].upper()
         try:
             patient = Patient.objects.get(health_id__iexact=health_id)
-            self.message.respond(_(u"%(patient)s; Household: %(household)s" % \
+            self.message.respond(_(u"SUCCESS: %(patient)s; Household: %(household)s" % \
                                 {'patient': patient, \
                                 'household': patient.household}))
             return True
@@ -43,16 +45,20 @@ class CheckHealthIdCommand(CCCommand):
         try:
             health_id_obj = HealthId.objects.get(health_id__iexact=health_id)
         except HealthId.DoesNotExist:
-            self.message.respond(_(u"Health ID %(id)s has not been assigned to a patient \
-                                    and does not exist as a valid health ID in the database.") % \
-                                    {'id': health_id.upper()})
+            resp = _(u"Health ID %(id)s does NOT exist in the database ") \
+                % {'id': health_id.upper()}
+            if checksum.is_valid_identifier(health_id):
+                resp += _(u" but IS a valid identifier.  Check to make sure the database is current.")
+            else:
+                resp += _(u" and is NOT a valid identifier.  You might have entered the ID incorrectly.")
+            self.message.respond(resp)
             return True
 
         resp = _(u"Health ID %(id)s ") % {'id': health_id.upper()}
         if health_id_obj.status == HealthId.STATUS_GENERATED:
-            resp += _(u" is valid but is not assigned to a patient.")
+            resp += _(u" is valid but no patient has been registered with this ID.")
         elif health_id_obj.status == HealthId.STATUS_PRINTED:
-            resp += _(u" is valid and has been printed out.")
+            resp += _(u" is valid but no patient has been registered with this ID.")
         elif health_id_obj.status == HealthId.STATUS_ISSUED:
             resp += _(u" is marked as belonging to a patient, but that \
                         patient is unknown.")
@@ -61,4 +67,5 @@ class CheckHealthIdCommand(CCCommand):
         else:
             raise BadValue("Health ID is marked with an invalid status.")
 
+        self.message.respond(resp)
         return True
