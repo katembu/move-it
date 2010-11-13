8423af8bd069b8e3ec5d72fbfee0f3dcaaf4968a
Ukang'a Dickson <ukanga@gmail.com>
2010-11-13 10:17:55 +0300

    alert/notify facilitators of rdt tests with symptoms

--- apps/childcount/forms/FeverForm.py
+++ apps/childcount/forms/FeverForm.py
@@ -7,11 +7,20 @@
 from datetime import datetime, timedelta
 
 from django.utils.translation import ugettext as _
+from django.contrib.auth.models import Group
 
 from childcount.forms.CCForm import CCForm
+from childcount.forms.DangerSignsForm import DangerSignsForm
+from childcount.forms.ReferralForm import ReferralForm
+from childcount.forms.MedicineGivenForm import MedicineGivenForm
 from childcount.models import Encounter
+from childcount.models import CodedItem
 from childcount.models.reports import FeverReport
+from childcount.models.reports import DangerSignsReport
+from childcount.models.reports import ReferralReport
+from childcount.models.reports import MedicineGivenReport
 from childcount.forms.utils import MultipleChoiceField
+from childcount.utils import send_msg
 from childcount.exceptions import ParseError, Inapplicable
 
 
@@ -21,6 +30,8 @@ class FeverForm(CCForm):
         'fr': ['f'],
     }
     ENCOUNTER_TYPE = Encounter.TYPE_PATIENT
+    _short_msg = ''
+    _fever_report = ''
 
     def process(self, patient):
         rdt_field = MultipleChoiceField()
@@ -78,8 +89,8 @@ class FeverForm(CCForm):
                                    {'age': yage, \
                                     'tabs': tabs, \
                                     'plural': (tabs > 1) and 's' or ''}
-
-            self.response = _("Positive RDT result. ") + instructions
+            self._short_msg = _("Positive RDT result. ")
+            self.response = self._short_msg + instructions
 
             '''alert = \
                 _("MRDT> Child %(last_name)s, %(first_name)s, "\
@@ -93,8 +104,55 @@ class FeverForm(CCForm):
             '''
         elif rdt == FeverReport.RDT_NEGATIVE:
             self.response = _(u"Negative RDT result.")
+            self._short_msg = self.response
         elif rdt == FeverReport.RDT_UNKOWN:
             self.response = _(u"Unkown RDT result.")
+            self._short_msg = self.response
 
         fr.rdt_result = rdt
         fr.save()
+        self._fever_report = fr
+
+    def post_process(self, forms_list):
+        danger_signs = ""
+        referred_str = ""
+        meds = ""
+        for form in forms_list:
+            # get the form object/class 
+            k = form['obj']
+            if isinstance(k, DangerSignsForm):
+                dsr = DangerSignsReport.objects.get(encounter=k.encounter)
+                fv = CodedItem.by_local_code(CodedItem.TYPE_DANGER_SIGN, 'fv')
+                only_fever = fv in dsr.danger_signs.all()
+                if dsr.danger_signs.all().count() == 1 and only_fever:
+                   pass
+                else:
+                    tmp = [ds.description for ds in dsr.danger_signs.all()]
+                    danger_signs = u", " . join(tmp)
+            if isinstance(k, ReferralForm):
+                rfr = ReferralReport.objects.get(encounter=k.encounter)
+                referred_str = rfr.get_urgency_display()
+            if isinstance(k, MedicineGivenForm):
+                mgr = MedicineGivenReport.objects.get(encounter=k.encounter)
+                medicines = mgr.medicines.all()
+                meds = _(u" Given: ")
+                meds += u", " . join([med.description for med in medicines])
+                meds += "."
+                
+        if danger_signs != "":
+            msg = _(u"%(patient)s has %(msg)s %(danger_signs)s.%(meds)s "
+                    "%(referral)s %(chw)s %(mobile)s." % {'patient':
+                                    self._fever_report.encounter.patient,
+                                    'msg': self._short_msg,
+                                    'danger_signs': danger_signs,
+                                    'referral': referred_str,
+                                    'meds': meds,
+                                    'chw' : self.chw,
+                                    'mobile': self.chw.connection().identity})
+            #alert facilitators
+            try:
+                g = Group.objects.get(name='Facilitator')
+                for user in g.user_set.all():
+                    send_msg(user.reporter, msg)
+            except Group.DoesNotExist:
+                pass
