de5f2efdcb3ed7891c0c74d848d2c05b0b617817
Alou Dolo <aloud7@gmail.com>
2010-12-20 13:08:13 +0000

    add UtilizationReport.py

---
+++ apps/childcount/reports/ondemand/UtilizationReport.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: alou
+
+
+from datetime import date
+
+from django.utils.translation import gettext_lazy as _
+
+from ccdoc import Document, Table, Paragraph, Text, Section
+
+from logger_ng.models import LoggedMessage
+
+from childcount.reports.utils import render_doc_to_file
+from childcount.reports.report_framework import PrintedReport
+
+def _(text):
+    """ short circuits the translation as not supported by CCdoc
+    """
+    return text
+
+def list_average(values):
+    total = 0
+    for value in values:
+        total += value
+    return int(total / values.__len__())
+
+def list_median(values):
+    sorted = values
+    sorted.sort()
+    num = sorted.__len__()
+    if num % 2 == 0:
+        half = num/2
+        center = sorted[num/2:(num/2)+2]
+        moy = list_average(center)
+    else:
+        moy = sorted[num+1/2]
+    return moy
+
+class Report(PrintedReport):
+    title = 'Utilization Report'
+    filename = 'UtilizationReport'
+    formats = ['html', 'pdf', 'xls']
+    argvs = []
+
+    def generate(self, rformat, title, filepath, data):
+        """ Display sms number per month, total, average, median"""
+
+        doc = Document(title)
+
+        header_row = [Text(_(u'Indicator:'))]
+        year_today = date.today().year
+        for month_num in range(1,13):
+            month = date(year=year_today, month=month_num, day=1).strftime("%B")
+            header_row.append(Text(month.title()))
+
+        header_row += [
+            Text(_(u'Total')),
+            Text(_(u'Average')),
+            Text(_(u'Median'))
+        ]
+
+        table = Table(header_row.__len__())
+        table.add_header_row(header_row)
+
+        doc.add_element(table)
+
+        # SMS NUMBER PER MONTH
+        list_sms = []
+        list_sms.append("SMS per month")
+
+        list_sms_month = []
+        for month_num in range(1,13):
+            sms_month = LoggedMessage.incoming.filter(date__month=month_num)
+            list_sms_month.append(sms_month.count())
+
+        list_sms += list_sms_month
+
+        total = LoggedMessage.incoming.all().count()
+        list_sms.append(total)
+
+        average = list_average(list_sms_month)
+        list_sms.append(average)
+
+        median = list_median(list_sms_month)
+        list_sms.append(median)
+
+        list_sms_text= [Text(sms) for sms in list_sms]
+        table.add_row(list_sms_text)
+        return render_doc_to_file(filepath, rformat, doc)
+
