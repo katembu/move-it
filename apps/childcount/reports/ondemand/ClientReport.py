bd237887595e15698f2c7a012083d16e96771532
yelema4 <yelema4@yelema4-laptop.(none)>
2011-01-27 16:20:47 +0000

    tache 1, 3, 6

--- apps/childcount/reports/ondemand/ClientReport.py
+++ apps/childcount/reports/ondemand/ClientReport.py
@@ -3,7 +3,7 @@
 # maintainer: diarra
 
 import calendar
-
+import time
 from datetime import datetime, date
 
 from django.utils.translation import gettext as _
@@ -103,15 +103,33 @@ def encounter_alert(nbr_DayAfterEncounter, b_FullName):
     icon = instruction_text = ""
 
     last_visit = nbr_DayAfterEncounter
+    date_before_overdue = ""
+    day_in_month = calendar.monthrange(date.today().year, date.today().month)[1]
 
     if nbr_DayAfterEncounter >= 60:
-        last_visit = nbr_DayAfterEncounter
-        b_FullName = b_LastVisit = True
-        exceded_days = nbr_DayAfterEncounter - 60
-        instruction_text = ("Visit HH by %s")%(exceded_days)
+        day_deadline = ""
+        month_deadline = ""
+        year_deadline = ""
+        x = date.today()
+        if (x.day + (90 - nbr_DayAfterEncounter)) < day_in_month:
+            day_deadline = x.day + (90 - nbr_DayAfterEncounter)
+            date_before_overdue = date(x.year, x.month, day_deadline)
 
-    if nbr_DayAfterEncounter >= 90:
-        last_visit = "! %s !" % nbr_DayAfterEncounter
+
+        else :
+            day_deadline = (x.day + (90 - nbr_DayAfterEncounter)) - day_in_month
+            month_deadline = x.month + 1
+            date_before_overdue = date(x.year, month_deadline, day_deadline)
+
+            if month_deadline > 12:
+                year_deadline = x.year + 1
+                date_before_overdue = date(year_deadline, month_deadline, day_deadline)
+
+        b_FullName = b_LastVisit = True
+        instruction_text = date_before_overdue.strftime(u'Visit HH by %d %b')
+
+    if nbr_DayAfterEncounter >= 3:
+        last_visit = "! %s !" % "Overdue"
         b_FullName = b_LastVisit = True
         icon = "!"
 
@@ -186,7 +204,8 @@ class Report(PrintedReport):
                    % {'clinic': chw.clinic,\
                       'full_name': chw.full_name()}
             else:
-                section_name = chw.full_name()
+                section_name = chw.full_name() + ' ' + time.strftime(u'Data from %b. 1 to %b. %d %Y')
+
 
             doc.add_element(Section(section_name))
 
@@ -225,7 +244,7 @@ class Report(PrintedReport):
                 table1.set_column_width(5, 6)
                 table1.set_column_width(4, 7)
                 table1.set_column_width(8, 8)
-                table1.set_column_width(5, 9)
+                table1.set_column_width(10, 9)
                 table1.set_column_width(5, 10)
 
                 table1.set_alignment(Table.ALIGN_LEFT, column=2)
@@ -244,7 +263,7 @@ class Report(PrintedReport):
                     .filter(encounter__patient__health_id=child\
                     .health_id).order_by('-encounter__encounter_date')
 
-                    rate_muac = '-'
+                    rate_muac = ''
 
                     if len(nutrition_report) > 1:
                         rate_muac = ((nutrition_report[0].muac \
@@ -263,9 +282,9 @@ class Report(PrintedReport):
                             .order_by('-encounter__encounter_date')[0]\
                             .muac
                     except NutritionReport.DoesNotExist:
-                        muac = '-'
+                        muac = ''
                     except IndexError:
-                        muac = '-'
+                        muac = ''
 
                     #Making an alerte if nutrition report change.
                     two_LastReport = []
@@ -318,7 +337,7 @@ class Report(PrintedReport):
                                              'muac': muac})
                         icon_rate = u"◆"
                     else:
-                        rate_muac = ('%(muac)s (%(rate_muac)s)' % \
+                        rate_muac = ('%(muac)s %(rate_muac)s' % \
                                             {'rate_muac': rate_muac, \
                                              'muac': muac})
                     child_muac = NutritionReport.objects\
