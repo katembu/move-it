4a81ade7204e12b5618adfa911d9b1f85cd77fe3
Ukang'a Dickson <ukanga@gmail.com>
2011-02-07 11:11:12 +0300

    added head section

--- apps/childcount/reports/definitions/PMTCTDefaulters.py
+++ apps/childcount/reports/definitions/PMTCTDefaulters.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from childcount.reports.pmtct import pmtct_defaulters
 from childcount.reports.utils import render_doc_to_file
--- apps/childcount/reports/definitions/PMTCTUpcomingAppointmets.py
+++ apps/childcount/reports/definitions/PMTCTUpcomingAppointmets.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
 
 from childcount.reports.pmtct import upcoming_appointments
 from childcount.reports.utils import render_doc_to_file
