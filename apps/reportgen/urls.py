bd3bc4cdbd852c6e7aeba383ce04c571804b1ded
Henry C-G <noemail@noemail>
2011-04-06 17:21:55 +0300

    Fix to static file serving

--- apps/reportgen/urls.py
+++ apps/reportgen/urls.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 
+import os
+
 from django.conf.urls.defaults import include, patterns, url
 from django.contrib import admin
 import webapp
@@ -12,5 +14,6 @@ urlpatterns = patterns('',
     (r'^reportgen/nightly/$', reportgen.views.nightly),
     (r'^reportgen/ondemand/$', reportgen.views.ondemand),
     url(r'^static/reportgen/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': 'apps/reportgen/static', 'show_indexes': False}),
+            {'document_root': '%s/static' % os.path.dirname(__file__),
+                'show_indexes': False}),
 )
