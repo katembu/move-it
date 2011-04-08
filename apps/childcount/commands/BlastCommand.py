cf2b09accad99102f870bdca6f8d52843f688b5d
Ukang'a Dickson <ukanga@gmail.com>
2011-04-08 05:12:28 +0300

    BlastCommand: broadcast mesages to all CHWs

---
+++ apps/childcount/commands/BlastCommand.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+'''
+    Broadcast message to all CHWs.
+'''
+
+from datetime import datetime, timedelta
+
+from django.utils.translation import ugettext as _
+
+from reversion import revision
+from reversion.models import Revision, Version
+from childcount.models.ccreports import TheBHSurveyReport
+
+from childcount.commands import CCCommand
+from childcount.utils import authenticated
+from childcount.utils import send_msg
+from childcount.exceptions import Inapplicable
+
+
+class BlastCommand(CCCommand):
+
+    KEYWORDS = {
+        'en': ['blast'],
+        'fr': ['blast'],
+    }
+
+    @authenticated
+    def process(self):
+        chw = self.message.persistant_connection.reporter.chw
+        # TODO: limit reporters who can send this
+        if self.params.__len__() > 1:
+            msg = u' ' . join(self.params[1:])
+            for chw in CHW.objects.all():
+                send_msg(chw, msg)
