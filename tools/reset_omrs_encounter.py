9df926bc2912744317729b4505d4699231a684cf
Moses Katembu <mose.kate@gmail.com>
2011-11-27 11:30:12 -0500

    Reset all encounters to fale to enable a new OMRS synchronization

---
+++ tools/reset_omrs_encounter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+
+###
+### START - SETUP RAPIDSMS ENVIRONMENT
+###
+
+import sys, os
+from os import path
+
+# figure out where all the extra libs (rapidsms and contribs) are
+libs=[os.path.abspath('lib'),os.path.abspath('apps')] # main 'rapidsms/lib'
+try:
+    for f in os.listdir('contrib'):
+        pkg = path.join('contrib',f)
+        if path.isdir(pkg) and \
+                'lib' in os.listdir(pkg):
+            libs.append(path.abspath(path.join(pkg,'lib')))
+except:
+    pass
+
+# add extra libs to the python sys path
+sys.path.extend(libs)
+path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(path)
+
+os.environ['RAPIDSMS_INI'] = os.path.join(path, "local.ini")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidsms.webui.settings'
+# import manager now that the path is correct
+from rapidsms import manager
+from django.db import IntegrityError
+from childcount.models import HealthId
+###
+### END - SETUP RAPIDSMS ENVIRONMENT
+###
+
+
+from reversion import revision
+from childcount.models import Encounter
+from datetime import datetime
+
+revision.start()
+
+i = 0
+for encounter in Encounter.objects.all():
+    i +=1
+    ''' Set new status to OMRS not Synched '''
+    encounter.sync_omrs = False
+    encounter.save()
+
+if i == 0:
+	print "No encounters " 
+else:
+	print "%d Encounters have been reset to False " % i
+
+revision.end()
+
+
+
